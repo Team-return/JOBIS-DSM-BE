6305f600ddc1aaf5078b7c71b06a9759b1d14345
keisa <kesksjxhx@gmail.com>
2022-10-19 23:08:49 +0900

    🗂 :: 프로젝트 생성

---
+++ src/test/java/com/example/jobis/JobisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
