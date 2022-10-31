a60a6dad40d571cb8701894718e44d4a729d6cc5
이하성 <tedsoftj1123@gmail.com>
2022-10-31 22:29:20 +0900

    🗂 :: 예외 핸들러, 필터

---
+++ src/main/java/com/example/jobis/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.example.jobis.global.error;
+
+import com.example.jobis.global.error.exception.ErrorCode;
+import com.example.jobis.global.error.exception.JobisException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(JobisException.class)
+    protected ResponseEntity<ErrorResponse> handleBaseException(JobisException e) {
+        log.warn("RuntimeException " + e);
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = ErrorResponse.builder()
+                .message(errorCode.getMessage())
+                .status(errorCode.getStatus())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(",   "));
+        ErrorResponse response = ErrorResponse.builder()
+                .status(400)
+                .message(msg)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
