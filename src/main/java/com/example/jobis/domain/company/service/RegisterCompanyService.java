13ab32af240115de270cfe5e827338e9ac7ed7a9
keisa <kesksjxhx@gmail.com>
2022-11-02 09:40:15 +0900

    🛠 :: 정리

--- src/main/java/com/example/jobis/domain/company/service/RegisterCompanyService.java
+++ src/main/java/com/example/jobis/domain/company/service/RegisterCompanyService.java
@@ -3,13 +3,7 @@ package com.example.jobis.domain.company.service;
 import com.example.jobis.domain.company.controller.dto.request.RegisterCompanyRequest;
 import com.example.jobis.domain.company.domain.Company;
 import com.example.jobis.domain.company.domain.repository.CompanyRepository;
-import com.example.jobis.domain.company.domain.type.Address;
-import com.example.jobis.domain.company.domain.type.CompanyInfo;
-import com.example.jobis.domain.company.domain.type.Contact;
-import com.example.jobis.domain.company.domain.type.Manager;
-import com.example.jobis.domain.company.exception.InvalidBusinessNumberException;
 import com.example.jobis.domain.company.facade.CompanyFacade;
-import com.example.jobis.infrastructure.resttemplate.facade.RestTemplateFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -31,32 +25,11 @@ public class RegisterCompanyService {
         }
 
         companyRepository.save(Company.builder()
+                .companyName(companyFacade.getCompanyName(request.getBusinessNumber()))
                 .businessNumber(request.getBusinessNumber())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .companyIntroduce(request.getCompanyIntroduce())
-                .companyName(companyFacade.getCompanyName(request.getBusinessNumber()))
-                .address(Address.builder()
-                        .zipCode1(request.getZipCode1())
-                        .address1(request.getAddress1())
-                        .zipCode2(request.getZipCode2())
-                        .address2(request.getAddress2())
-                        .build())
-                .manager(Manager.builder()
-                        .manager1(request.getManager1())
-                        .phoneNumber1(request.getPhoneNumber1())
-                        .manager2(request.getManager2())
-                        .phoneNumber2(request.getPhoneNumber2())
-                        .build())
-                .contact(Contact.builder()
-                        .fax(request.getFax())
-                        .email(request.getEmail())
-                        .build())
-                .companyInfo(CompanyInfo.builder()
-                        .representativeName(request.getRepresentativeName())
-                        .foundedAt(request.getFoundedAt())
-                        .workerNumber(request.getWorkerNumber())
-                        .take(request.getTake())
-                        .build())
                 .build());
+
+        //토큰 반환 추가해야함
     }
 }
