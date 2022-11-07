ed71857ecc81ad088e04757585c83008b64817dc
이하성 <tedsoftj1123@gmail.com>
2022-11-07 19:43:08 +0900

    ⚙️ :: basetime entity

--- src/main/java/com/example/jobis/global/entity/BaseTimeEntity.java
+++ src/main/java/com/example/jobis/global/entity/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
@@ -11,10 +12,9 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
-    @NotNull
     @CreatedDate
-    private LocalDateTime createdAt;
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
