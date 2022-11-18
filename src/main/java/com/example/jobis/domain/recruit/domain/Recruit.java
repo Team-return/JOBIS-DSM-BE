cb960a894663978849cc50d0e4b5790627ee0798
이하성 <tedsoftj1123@gmail.com>
2022-11-18 17:33:45 +0900

    🧩 :: 모집의뢰 엔티티 작성

--- src/main/java/com/example/jobis/domain/recruit/domain/Recruit.java
+++ src/main/java/com/example/jobis/domain/recruit/domain/Recruit.java
@@ -13,23 +13,32 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Recruit extends BaseTimeEntity {
+public class Recruit extends BaseTimeEntity{
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "recruit_id")
     private Long id;
 
+    @Column(nullable = false)
     private Year recruitYear;
 
     @Enumerated(EnumType.STRING)
     private RecruitStatus status;
 
+    @Column(length = 100)
     private String benefit;
 
-    private boolean to;
+    @Column(columnDefinition = "BIT(1)", nullable = false)
+    private boolean military;
+
+    @Column(length = 100)
+    private String etc;
 
     @Embedded
     private RecruitDate recruitDate;
@@ -38,18 +47,26 @@ public class Recruit extends BaseTimeEntity {
     private Pay pay;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "company_id")
+    @JoinColumn(name = "company_id", nullable = false)
     private Company company;
+
+    @OneToMany(mappedBy = "recruit")
+    private List<RecruitArea> recruitAreaList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "recruit")
+    private List<RequiredLicences> requiredLicencesList = new ArrayList<>();
 
     @Builder
     public Recruit(Year year, RecruitStatus status, Integer trainPay, Integer pay,
-                   LocalDate startDate, LocalDate endDate, Company company, String benefit, boolean to) {
+                   LocalDate startDate, LocalDate endDate, Company company, String benefit, boolean military, String etc
+    ) {
         this.recruitYear = year;
         this.status = status;
         this.benefit = benefit;
         this.recruitDate = new RecruitDate(startDate, endDate);
         this.pay = new Pay(trainPay, pay);
         this.company = company;
-        this.to = to;
+        this.military = military;
+        this.etc = etc;
     }
 }
