e99a81fb5756ce150b84c4d7e49695682d34eb30
keisa <kesksjxhx@gmail.com>
2022-11-18 20:15:45 +0900

    🛩 :: import 정리

--- src/main/java/com/example/jobis/domain/company/controller/CompanyController.java
+++ src/main/java/com/example/jobis/domain/company/controller/CompanyController.java
@@ -8,10 +8,8 @@ import com.example.jobis.domain.company.controller.dto.request.CompanyDetailsReq
 import com.example.jobis.domain.company.controller.dto.request.RegisterCompanyRequest;
 import com.example.jobis.domain.company.service.CreateCompanyDetailsService;
 import com.example.jobis.domain.company.service.RegisterCompanyService;
-import com.example.jobis.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
