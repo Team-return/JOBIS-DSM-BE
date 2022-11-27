a869d25d57c081eaac6c9ea52baff9dcd1b55a19
gilgeunwoo <kesksjxhx@gmail.com>
2022-11-28 08:44:31 +0900

    🗂 :: 학생 엔티티 추가

---
+++ src/main/java/com/example/jobis/domain/student/domain/Student.java
@@ -0,0 +1,39 @@
+package com.example.jobis.domain.student.domain;
+
+import com.example.jobis.domain.student.domain.types.Gender;
+import com.example.jobis.domain.student.domain.types.Grade;
+import com.example.jobis.domain.user.domain.User;
+import com.example.jobis.global.enums.Authority;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Student extends User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Builder
+    public Student(String accountId, String password, String name, Grade grade, Gender gender) {
+        super(accountId, password, Authority.STUDENT);
+        this.name = name;
+        this.grade = grade;
+        this.gender = gender;
+    }
+}
