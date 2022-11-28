39a39d7322fe3e933111a9ce23db51d5783698cf
이하성 <tedsoftj1123@gmail.com>
2022-11-28 09:32:37 +0900

    🧩 :: 코드 유효성 검사 메서드

---
+++ src/main/java/com/example/jobis/domain/code/facade/CodeFacade.java
@@ -0,0 +1,26 @@
+package com.example.jobis.domain.code.facade;
+
+import com.example.jobis.domain.code.domain.Code;
+import com.example.jobis.domain.code.domain.enums.CodeType;
+import com.example.jobis.domain.code.domain.repository.CodeRepository;
+import com.example.jobis.domain.code.exception.InvalidCodeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CodeFacade {
+    private final CodeRepository codeRepository;
+
+    public List<Code> findCodesWithValidation(List<Long> codes, CodeType codeType) {
+        List<Code> resCodes = new ArrayList<>();
+        codes.forEach(
+                c -> resCodes.add(codeRepository.findByCodeTypeAndCode(codeType, c)
+                        .orElseThrow(()->InvalidCodeException.EXCEPTION))
+        );
+        return resCodes;
+    }
+}
--- src/main/java/com/example/jobis/domain/code/service/FindCodeService.java
+++ src/main/java/com/example/jobis/domain/code/service/FindCodeService.java
@@ -6,6 +6,7 @@ import com.example.jobis.domain.code.domain.repository.CodeRepository;
 import com.example.jobis.domain.code.presentaion.dto.response.CodeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 public class FindCodeService {
     private final CodeRepository codeRepository;
 
+    @Transactional(readOnly = true)
     public List<CodeResponse> execute(String keyword, CodeType type) {
         return codeRepository.findByKeywordStartsWithAndCodeType(keyword, type)
                 .stream()
