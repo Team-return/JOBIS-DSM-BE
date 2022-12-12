dfa15fb3e66b378755c2b5b25fabbaedc839de41
gilgeunwoo <kesksjxhx@gmail.com>
2022-12-12 13:33:44 +0900

    🗂️ :: AuthCode Entity 생성

---
+++ src/main/java/com/example/jobis/domain/student/domain/AuthCode.java
@@ -0,0 +1,33 @@
+package com.example.jobis.domain.student.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@Builder
+@RedisHash
+public class AuthCode {
+
+     @Id
+    private final String email;
+
+     private String code;
+
+     private boolean isVerified;
+
+     @TimeToLive
+    private Long ttl;
+
+     public void updateAuthCode(String code, Long ttl) {
+         this.code = code;
+         this.ttl = ttl;
+         this.isVerified = false;
+     }
+
+     public void verify() {
+         this.isVerified = true;
+     }
+}
