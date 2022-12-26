fafb214c667075656160cc5be3ef4a131d1ba3eb
이하성 <tedsoftj1123@gmail.com>
2022-12-26 17:23:50 +0900

    🧩 :: 토큰 재발급 기능

--- src/main/java/com/example/jobis/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/jobis/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.example.jobis.domain.auth.domain.repository;
 import com.example.jobis.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
 }
--- src/main/java/com/example/jobis/global/redis/RedisConfig.java
+++ src/main/java/com/example/jobis/global/redis/RedisConfig.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @EnableRedisRepositories
@@ -25,9 +27,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+
 }
