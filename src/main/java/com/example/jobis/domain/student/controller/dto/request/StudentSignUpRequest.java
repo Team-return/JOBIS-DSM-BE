0dd754efbaada1bc13e28c49181aaf92f1baf0d7
gilgeunwoo <kesksjxhx@gmail.com>
2022-12-27 19:38:16 +0900

    🗂️ :: 유효성 검사 추가

--- src/main/java/com/example/jobis/domain/student/controller/dto/request/StudentSignUpRequest.java
+++ src/main/java/com/example/jobis/domain/student/controller/dto/request/StudentSignUpRequest.java
@@ -2,16 +2,28 @@ package com.example.jobis.domain.student.controller.dto.request;
 
 import com.example.jobis.domain.student.domain.types.Gender;
 import com.example.jobis.domain.student.domain.types.Grade;
+import com.example.jobis.global.util.RegexProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class StudentSignUpRequest {
 
+    @Pattern(regexp = RegexProperty.EMAIL, message = "이메일은 @dsm.hs.kr을 포함해야함")
     private String accountId;
+
+    @Pattern(regexp = RegexProperty.PASSWORD, message = "비밀번호는 '숫자', '문자', '특수문자' 무조건 1개 이상, 비밀번호 '최소 8자에서 최대 16자'까지 허용")
     private String password;
+    @NotBlank
     private Grade grade;
+    @NotBlank
     private String name;
+    @NotBlank
     private Gender gender;
 }
