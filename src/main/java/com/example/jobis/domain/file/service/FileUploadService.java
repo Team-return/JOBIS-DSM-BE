6950b0694489542cd411ff00d1265a629bc4632a
이하성 <tedsoftj1123@gmail.com>
2022-12-27 20:14:24 +0900

    🧩 :: 파일 업로드 구현

---
+++ src/main/java/com/example/jobis/domain/file/service/FileUploadService.java
@@ -0,0 +1,17 @@
+package com.example.jobis.domain.file.service;
+
+import com.example.jobis.domain.file.presentation.type.FileType;
+import com.example.jobis.infrastructure.s3.S3Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class FileUploadService {
+    private final S3Util s3Util;
+
+    public String execute(MultipartFile multipartFile, FileType fileType) {
+        return s3Util.uploadImg(multipartFile, fileType);
+    }
+}
