ea9c9af3500737acd1908d5a82afec4762cb4053
gilgeunwoo <kesksjxhx@gmail.com>
2023-01-01 22:49:08 +0900

    🛩️ :: CodeResponse : AllArgs -> Builder 어노테이션으로 변경

--- src/main/java/com/example/jobis/domain/code/controller/dto/response/CodeResponse.java
+++ src/main/java/com/example/jobis/domain/code/controller/dto/response/CodeResponse.java
@@ -1,10 +1,10 @@
 package com.example.jobis.domain.code.controller.dto.response;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class CodeResponse {
     private final Long code;
     private final String keyword;
