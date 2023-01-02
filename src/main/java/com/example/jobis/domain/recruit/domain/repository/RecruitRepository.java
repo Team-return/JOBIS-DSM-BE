708e25ac1af43d960c4f872c2b71bf956d8d70f0
gilgeunwoo <kesksjxhx@gmail.com>
2023-01-02 22:13:13 +0900

    🗂️ :: 쿼리 함수 추가

--- src/main/java/com/example/jobis/domain/recruit/domain/repository/RecruitRepository.java
+++ src/main/java/com/example/jobis/domain/recruit/domain/repository/RecruitRepository.java
@@ -3,5 +3,9 @@ package com.example.jobis.domain.recruit.domain.repository;
 import com.example.jobis.domain.recruit.domain.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+
+    Optional<Recruit> findByIdAndRecruitYear(Long id, int recruitYear);
 }
---
+++ src/main/java/com/example/jobis/domain/recruit/facade/RecruitFacade.java
@@ -0,0 +1,22 @@
+package com.example.jobis.domain.recruit.facade;
+
+import com.example.jobis.domain.recruit.domain.Recruit;
+import com.example.jobis.domain.recruit.domain.repository.RecruitRepository;
+import com.example.jobis.domain.recruit.exception.RecruitNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Component
+public class RecruitFacade {
+
+    private final RecruitRepository recruitRepository;
+
+    public Recruit getRecruitById(Long id, int year) {
+        return recruitRepository.findByIdAndRecruitYear(id, year)
+                .orElseThrow(() -> RecruitNotFoundException.EXCEPTION);
+    }
+}
