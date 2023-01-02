6dbf26e9c0abe1a78748d05ee4bb91cc4a4f504d
gilgeunwoo <kesksjxhx@gmail.com>
2023-01-02 22:15:49 +0900

    🛠 :: 안쓰는 import 제거

--- src/main/java/com/example/jobis/domain/recruit/controller/dto/request/ApplyRecruitmentRequest.java
+++ src/main/java/com/example/jobis/domain/recruit/controller/dto/request/ApplyRecruitmentRequest.java
@@ -3,9 +3,7 @@ package com.example.jobis.domain.recruit.controller.dto.request;
 import com.example.jobis.domain.recruit.domain.enums.ProgressType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.aspectj.apache.bcel.classfile.Code;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
