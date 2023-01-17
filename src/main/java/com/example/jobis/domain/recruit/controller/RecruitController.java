50a31ca406cb663f8bbd7565d32fca45b8d1eeb7
gilgeunwoo <kesksjxhx@gmail.com>
2023-01-17 17:27:31 +0900

    🧩 :: 모집 의뢰서 수정  api 추가

--- src/main/java/com/example/jobis/domain/recruit/controller/RecruitController.java
+++ src/main/java/com/example/jobis/domain/recruit/controller/RecruitController.java
@@ -1,9 +1,11 @@
 package com.example.jobis.domain.recruit.controller;
 
 import com.example.jobis.domain.recruit.controller.dto.request.ApplyRecruitmentRequest;
+import com.example.jobis.domain.recruit.controller.dto.request.UpdateRecruitmentRequest;
 import com.example.jobis.domain.recruit.controller.dto.response.RecruitListResponse;
 import com.example.jobis.domain.recruit.service.ApplyRecruitmentService;
 import com.example.jobis.domain.recruit.service.QueryRecruitListService;
+import com.example.jobis.domain.recruit.service.UpdateRecruitmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +17,18 @@ import javax.validation.Valid;
 @RequestMapping("/recruit")
 public class RecruitController {
     private final ApplyRecruitmentService applyRecruitmentService;
-    private final QueryRecruitListService queryRecruitListService;
+
+    private final UpdateRecruitmentService updateRecruitmentService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void applyRecruitment(@RequestBody @Valid ApplyRecruitmentRequest request) {
         applyRecruitmentService.execute(request);
     }
-    
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{recruit-id}")
+    public void updateRecruitment(@RequestBody @Valid UpdateRecruitmentRequest request, @PathVariable("recruit-id") Long recruitId) {
+        updateRecruitmentService.execute(request, recruitId);
+    }
 }
---
+++ src/main/java/com/example/jobis/domain/recruit/controller/dto/request/UpdateRecruitmentRequest.java
@@ -0,0 +1,47 @@
+package com.example.jobis.domain.recruit.controller.dto.request;
+
+import com.example.jobis.domain.recruit.domain.enums.ProgressType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class UpdateRecruitmentRequest {
+    @NotNull(message = "areas는 null일 수 없습니다.")
+    private List<Area> areas;
+    private String preferentialTreatment;
+    private Integer requiredGrade;
+
+    @NotNull(message = "work_hours는 null일 수 없습니다.")
+    private Integer workHours;
+    private List<String> requiredLicenses;
+
+    @NotNull(message = "hiring_progress는 null일 수 없습니다.")
+    private List<ProgressType> hiringProgress;
+    @NotNull(message = "train_pay는 null 일 수 없습니다.")
+    private Integer trainPay;
+    private Integer pay;
+    private String benefits;
+
+    @NotNull(message = "military는 null일 수 없습니다.")
+    private boolean military;
+    @NotNull(message = "submit_document는 null일 수 없습니다.")
+    private String submitDocument;
+    @NotNull(message = "start_date는 null일 수 없습니다.")
+    private LocalDate startDate;
+    @NotNull(message = "end_date는 null일 수 없습니다.")
+    private LocalDate endDate;
+    private String etc;
+
+    @Getter
+    public static class Area{
+        private List<Long> job;
+        private List<Long> tech;
+        private int hiring;
+        private String majorTask;
+    }
+}
