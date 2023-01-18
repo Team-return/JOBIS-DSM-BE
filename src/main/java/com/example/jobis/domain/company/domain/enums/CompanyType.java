b3210f48c0321d457b15ce7e97b86aeed8e2b5e8
이하성 <tedsoftj1123@gmail.com>
2023-01-18 11:46:32 +0900

    🗑 :: Default타입 삭제

--- src/main/java/com/example/jobis/domain/company/domain/enums/CompanyType.java
+++ src/main/java/com/example/jobis/domain/company/domain/enums/CompanyType.java
@@ -3,7 +3,6 @@ package com.example.jobis.domain.company.domain.enums;
 public enum CompanyType {
     LEAD, //선도기업
     PARTICIPATING, //참여기업
-    CONTRACTING, // 협약기업
-    DEFAULT
+    CONTRACTING // 협약기업
 
 }
