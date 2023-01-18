915a712d6f10279fc8e553cc894a8986c3c030b4
이하성 <tedsoftj1123@gmail.com>
2023-01-18 17:10:24 +0900

    🧩 :: batchsize

--- src/main/java/com/example/jobis/domain/recruit/domain/Recruitment.java
+++ src/main/java/com/example/jobis/domain/recruit/domain/Recruitment.java
@@ -9,6 +9,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -17,6 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@BatchSize(size = 200)
+@DynamicUpdate
+@DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Recruitment extends BaseTimeEntity{
