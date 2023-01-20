7209d4d5967ebdec2626daf6f90aae2e75f2c130
이하성 <tedsoftj1123@gmail.com>
2023-01-20 20:57:02 +0900

    🗂️ :: querydsl 설정

---
+++ src/main/java/com/example/jobis/global/querydsl/QuerydslConfig.java
@@ -0,0 +1,19 @@
+package com.example.jobis.global.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
