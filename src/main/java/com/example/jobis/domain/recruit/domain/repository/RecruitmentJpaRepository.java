4997dd9442b245e0744d9ec74260ba49c7935c82
이하성 <tedsoftj1123@gmail.com>
2023-01-21 15:23:28 +0900

    🛠 :: JpaRepository로 변환

--- src/main/java/com/example/jobis/domain/recruit/domain/repository/RecruitmentJpaRepository.java
+++ src/main/java/com/example/jobis/domain/recruit/domain/repository/RecruitmentJpaRepository.java
@@ -3,5 +3,5 @@ package com.example.jobis.domain.recruit.domain.repository;
 import com.example.jobis.domain.recruit.domain.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+public interface RecruitmentJpaRepository extends JpaRepository<Recruitment, Long> {
 }
