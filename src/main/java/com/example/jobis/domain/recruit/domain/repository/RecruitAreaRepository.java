f945eb18da82a06b14433ccb2c19e41369e3b316
이하성 <tedsoftj1123@gmail.com>
2023-01-21 18:06:48 +0900

    🧩 :: 모집의뢰 삭제시 연관 엔티티 연쇄삭제

--- src/main/java/com/example/jobis/domain/recruit/domain/repository/RecruitAreaRepository.java
+++ src/main/java/com/example/jobis/domain/recruit/domain/repository/RecruitAreaRepository.java
@@ -1,12 +1,8 @@
 package com.example.jobis.domain.recruit.domain.repository;
 
-import com.example.jobis.domain.recruit.domain.Recruitment;
 import com.example.jobis.domain.recruit.domain.RecruitArea;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface RecruitAreaRepository extends JpaRepository<RecruitArea, Long> {
-
-    List<RecruitArea> findAllByRecruitment(Recruitment recruitment);
 }
