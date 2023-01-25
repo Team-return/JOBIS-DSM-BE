02859fb288afcdfd1b28aaa6607a3e9bd29121cb
이하성 <tedsoftj1123@gmail.com>
2023-01-25 22:23:08 +0900

    🧩 :: delete file 매핑

---
+++ src/main/java/com/example/jobis/domain/file/presentation/dto/request/DeleteFileRequest.java
@@ -0,0 +1,11 @@
+package com.example.jobis.domain.file.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class DeleteFileRequest {
+    private String path;
+}
