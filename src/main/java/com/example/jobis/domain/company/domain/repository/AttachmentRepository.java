0ef7c0d075791f769394b4ac1a2c980dd92b00e5
gilgeunwoo <kesksjxhx@gmail.com>
2023-02-02 17:25:04 +0900

    🛠️ :: Attachment -> CompanyAttachment

--- src/main/java/com/example/jobis/domain/company/domain/repository/AttachmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jobis.domain.company.domain.repository;
-
-import com.example.jobis.domain.company.domain.Attachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-}
