8f5891f1245e1e5116f173c163c6490c6253d2e3
이하성 <tedsoftj1123@gmail.com>
2023-02-06 17:27:27 +0900

    🛩️ :: DTO병합

--- src/main/java/com/example/jobis/domain/company/controller/dto/response/CompanyListResponse.java
+++ src/main/java/com/example/jobis/domain/company/controller/dto/response/CompanyListResponse.java
@@ -1,6 +1,8 @@
 package com.example.jobis.domain.company.controller.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
@@ -9,5 +11,15 @@ import java.util.List;
 @AllArgsConstructor
 public class CompanyListResponse {
 
-    private final List<CompanyResponse> companyList;
+    private final String companyName;
+    private final String companyProfileUrl;
+    private final int take;
+
+    @Builder
+    @QueryProjection
+    public CompanyListResponse(String companyName, String companyProfileUrl, int take) {
+        this.companyName = companyName;
+        this.companyProfileUrl = companyProfileUrl;
+        this.take = take;
+    }
 }
--- src/main/java/com/example/jobis/domain/company/domain/repository/CompanyRepository.java
+++ src/main/java/com/example/jobis/domain/company/domain/repository/CompanyRepository.java
@@ -1,7 +1,7 @@
 package com.example.jobis.domain.company.domain.repository;
 
-import com.example.jobis.domain.company.controller.dto.response.CompanyResponse;
-import com.example.jobis.domain.company.controller.dto.response.QCompanyResponse;
+import com.example.jobis.domain.company.controller.dto.response.CompanyListResponse;
+import com.example.jobis.domain.company.controller.dto.response.QCompanyListResponse;
 import com.example.jobis.domain.company.domain.Company;
 import com.example.jobis.domain.company.exception.CompanyNotFoundException;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 import static com.example.jobis.domain.company.domain.QCompany.company;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,10 +18,10 @@ public class CompanyRepository {
     private final CompanyJpaRepository companyJpaRepository;
     private final JPAQueryFactory queryFactory;
 
-    public List<CompanyResponse> findCompanyInfoList() {
+    public List<CompanyListResponse> findCompanyInfoList() {
         return queryFactory
                 .select(
-                        new QCompanyResponse(
+                        new QCompanyListResponse(
                              company.name,
                              company.companyLogoUrl,
                              company.sales
