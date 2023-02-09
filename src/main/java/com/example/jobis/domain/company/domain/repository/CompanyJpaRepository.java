6be2510dd53ef98a947bbac1580d74593d9a6b81
이하성 <tedsoftj1123@gmail.com>
2023-02-09 23:53:11 +0900

    🧩 :: uuid로 변경

--- src/main/java/com/example/jobis/domain/company/domain/repository/CompanyJpaRepository.java
+++ src/main/java/com/example/jobis/domain/company/domain/repository/CompanyJpaRepository.java
@@ -4,8 +4,9 @@ import com.example.jobis.domain.company.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface CompanyJpaRepository extends JpaRepository<Company, Long> {
+public interface CompanyJpaRepository extends JpaRepository<Company, UUID> {
     Optional<Company> findByBizNo(String bizNo);
 
     boolean existsByBizNo(String bizNo);
--- src/main/java/com/example/jobis/domain/company/service/QueryCompanyDetailsService.java
+++ src/main/java/com/example/jobis/domain/company/service/QueryCompanyDetailsService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
 public class QueryCompanyDetailsService {
@@ -14,7 +16,7 @@ public class QueryCompanyDetailsService {
     private final CompanyFacade companyFacade;
 
     @Transactional(readOnly = true)
-    public CompanyDetailsResponse execute(Long companyId) {
+    public CompanyDetailsResponse execute(UUID companyId) {
         Company company = companyFacade.getCompanyById(companyId);
 
         return CompanyDetailsResponse.builder()
