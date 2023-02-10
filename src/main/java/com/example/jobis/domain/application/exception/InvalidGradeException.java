7e8521a260c8059e5c2ce97019e3f850e70f3e49
gilgeunwoo <kesksjxhx@gmail.com>
2023-02-10 14:59:47 +0900

    🛠️ :: exception 명 변경

--- src/main/java/com/example/jobis/domain/application/exception/InvalidGradeException.java
+++ src/main/java/com/example/jobis/domain/application/exception/InvalidGradeException.java
@@ -3,11 +3,11 @@ package com.example.jobis.domain.application.exception;
 import com.example.jobis.global.error.exception.ErrorCode;
 import com.example.jobis.global.error.exception.JobisException;
 
-public class NotThirdGradeException extends JobisException {
+public class InvalidGradeException extends JobisException {
 
-    public static final JobisException EXCEPTION = new NotThirdGradeException();
+    public static final JobisException EXCEPTION = new InvalidGradeException();
 
-    private NotThirdGradeException() {
+    private InvalidGradeException() {
         super(ErrorCode.NOT_THIRD_GRADE);
     }
 }
