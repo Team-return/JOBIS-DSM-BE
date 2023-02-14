7cd14ed919ca05b094b4dced6428240de712bdac
gilgeunwoo <kesksjxhx@gmail.com>
2023-02-14 14:12:23 +0900

    🛠️ :: 코드 줄이기

--- src/main/java/com/example/jobis/domain/application/service/DeleteApplicationService.java
+++ src/main/java/com/example/jobis/domain/application/service/DeleteApplicationService.java
@@ -4,6 +4,7 @@ import com.example.jobis.domain.application.domain.Application;
 import com.example.jobis.domain.application.domain.enums.ApplicationStatus;
 import com.example.jobis.domain.application.domain.repository.ApplicationRepository;
 import com.example.jobis.domain.application.exception.ApplicationAlreadyExistsException;
+import com.example.jobis.domain.application.exception.InvalidStudentException;
 import com.example.jobis.domain.student.domain.Student;
 import com.example.jobis.domain.student.facade.StudentFacade;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +24,15 @@ public class DeleteApplicationService {
     public void execute(UUID applicationId) {
 
         Student student = studentFacade.getCurrentStudent();
-        Application application = applicationRepository.findApplicationByIdAndStudent(applicationId, student);
+        Application application = applicationRepository.findApplicationById(applicationId);
         ApplicationStatus status = application.getApplicationStatus();
 
-        if (status.equals(ApplicationStatus.APPROVED) || status.equals(ApplicationStatus.FAILED)
-                || status.equals(ApplicationStatus.PASS)) {
+        if (!status.equals(ApplicationStatus.REQUESTED)) {
             throw ApplicationAlreadyExistsException.EXCEPTION;
+        }
+
+        if (!application.getStudent().equals(student)) {
+            throw InvalidStudentException.EXCEPTION;
         }
 
         applicationRepository.deleteApplication(application);
