4e23e4fc61edbe559355f3682ec1490ddc802c6b
gilgeunwoo <kesksjxhx@gmail.com>
2023-02-15 18:41:49 +0900

    🛠️ :: UUID, int -> Integer

--- src/main/java/com/example/jobis/domain/recruit/controller/dto/response/StudentRecruitDetailsResponse.java
+++ src/main/java/com/example/jobis/domain/recruit/controller/dto/response/StudentRecruitDetailsResponse.java
@@ -1,31 +1,25 @@
 package com.example.jobis.domain.recruit.controller.dto.response;
 
-import com.example.jobis.domain.code.domain.Code;
-import com.example.jobis.domain.code.domain.RecruitAreaCode;
-import com.example.jobis.domain.code.domain.enums.CodeType;
-import com.example.jobis.domain.code.facade.CodeFacade;
-import com.example.jobis.domain.recruit.domain.RecruitArea;
-import com.example.jobis.domain.recruit.domain.enums.ProgressType;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Getter
 @Builder
-public class RecruitDetailsResponse {
+public class StudentRecruitDetailsResponse {
 
     private List<RecruitAreaResponse> areas;
     private String preferentialTreatment;
-    private int requiredGrade;
+    private Integer requiredGrade;
     private int workHours;
     private String requiredLicenses;
 
     private String hiringProgress;
-    private int trainPay;
-    private int pay;
+    private Integer trainPay;
+    private Integer pay;
     private String benefits;
     private boolean military;
     private String submitDocument;
@@ -36,7 +30,7 @@ public class RecruitDetailsResponse {
     @Getter
     @Builder
     public static class RecruitAreaResponse {
-        private Long recruitAreaId;
+        private UUID recruitAreaId;
         private List<String> job;
         private List<String> tech;
         private int hiring;
