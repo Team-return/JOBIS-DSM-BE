bb9b19926062714bea032ed3a7b909e364bfcd16
이하성 <tedsoftj1123@gmail.com>
2023-03-22 20:08:19 +0900

    🧩 :: (#156) 모집분야 삭제 서비스

---
+++ src/main/java/team/returm/jobis/domain/recruitment/service/DeleteRecruitAreaService.java
@@ -0,0 +1,31 @@
+package team.returm.jobis.domain.recruitment.service;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import team.returm.jobis.domain.recruitment.domain.RecruitArea;
+import team.returm.jobis.domain.recruitment.domain.repository.RecruitmentRepository;
+import team.returm.jobis.domain.recruitment.exception.RecruitAreaNotFoundException;
+import team.returm.jobis.domain.user.domain.User;
+import team.returm.jobis.domain.user.domain.enums.Authority;
+import team.returm.jobis.domain.user.facade.UserFacade;
+import team.returm.jobis.global.annotation.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteRecruitAreaService {
+
+    private final UserFacade userFacade;
+    private final RecruitmentRepository recruitmentRepository;
+
+    public void execute(UUID recruitAreaId) {
+        User user = userFacade.getCurrentUser();
+
+        RecruitArea recruitArea = recruitmentRepository.queryRecruitAreaById(recruitAreaId)
+                .orElseThrow(() -> RecruitAreaNotFoundException.EXCEPTION);
+        if (user.getAuthority() == Authority.COMPANY) {
+            recruitArea.getRecruitment().checkCompany(user.getId());
+        }
+
+        recruitmentRepository.deleteRecruitAreaById(recruitArea.getId());
+    }
+}
