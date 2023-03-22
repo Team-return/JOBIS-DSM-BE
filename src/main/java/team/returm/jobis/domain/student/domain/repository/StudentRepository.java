2364a96c8c388bf842cc0ecbd54056a8e5d9d743
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-22 20:12:56 +0900

    🗂️ :: (#154) 쿼리 함수

--- src/main/java/team/returm/jobis/domain/student/domain/repository/StudentRepository.java
+++ src/main/java/team/returm/jobis/domain/student/domain/repository/StudentRepository.java
@@ -15,4 +15,8 @@ public class StudentRepository {
     public Optional<Student> queryStudentById(UUID studentId) {
         return studentJpaRepository.findById(studentId);
     }
+
+    public Optional<Student> queryStudentByEmail(String email) {
+        return studentJpaRepository.findByEmail(email);
+    }
 }
