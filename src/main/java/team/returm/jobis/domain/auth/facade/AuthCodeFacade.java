5be42693e5e03bd975344996dd081aeeda29d733
gilgeunwoo <kesksjxhx@gmail.com>
2023-03-24 11:08:47 +0900

    🛠️ :: (#155) 함수 위치변경

--- src/main/java/team/returm/jobis/domain/auth/facade/AuthCodeFacade.java
+++ src/main/java/team/returm/jobis/domain/auth/facade/AuthCodeFacade.java
@@ -1,18 +1,13 @@
 package team.returm.jobis.domain.auth.facade;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import team.returm.jobis.domain.auth.domain.AuthCode;
 import team.returm.jobis.domain.student.domain.repository.AuthCodeRepository;
 import team.returm.jobis.domain.student.exception.BadAuthCodeException;
-import team.returm.jobis.domain.student.exception.BadEmailException;
-import team.returm.jobis.domain.student.exception.StudentAlreadyExistsException;
-import team.returm.jobis.domain.student.exception.StudentNotFoundException;
 import team.returm.jobis.domain.student.exception.UnverifiedEmailException;
-import team.returm.jobis.domain.student.facade.StudentFacade;
-import team.returm.jobis.global.util.RegexProperty;
 import team.returm.jobis.global.util.jms.JmsProperties;
 import team.returm.jobis.global.util.jms.JmsUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
 
@@ -24,23 +19,7 @@ public class AuthCodeFacade {
     private final AuthCodeRepository authCodeRepository;
     private final JmsUtil jmsUtil;
     private final JmsProperties jmsProperties;
-    private final StudentFacade studentFacade;
 
-    public void sendSignUpAuthCode(String email) {
-
-        if (studentFacade.existsEmail(email)) {
-            throw StudentAlreadyExistsException.EXCEPTION;
-        }
-        sendMail(email);
-    }
-
-    public void sendPasswordAuthCode(String email) {
-
-        if (!studentFacade.existsEmail(email)) {
-            throw StudentNotFoundException.EXCEPTION;
-        }
-        sendMail(email);
-    }
 
     public void verifyAuthCode(String code, String email) {
         AuthCode authCode = authCodeRepository.findById(email)
--- src/main/java/team/returm/jobis/domain/auth/service/SendAuthCodeService.java
+++ src/main/java/team/returm/jobis/domain/auth/service/SendAuthCodeService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import team.returm.jobis.domain.auth.domain.types.AuthCodeType;
 import team.returm.jobis.domain.auth.facade.AuthCodeFacade;
 import team.returm.jobis.domain.auth.presentation.dto.request.SendAuthCodeRequest;
+import team.returm.jobis.domain.student.exception.StudentAlreadyExistsException;
+import team.returm.jobis.domain.student.exception.StudentNotFoundException;
+import team.returm.jobis.domain.student.facade.StudentFacade;
 import team.returm.jobis.global.annotation.Service;
 
 @RequiredArgsConstructor
@@ -11,13 +14,30 @@ import team.returm.jobis.global.annotation.Service;
 public class SendAuthCodeService {
 
     private final AuthCodeFacade authCodeFacade;
+    private final StudentFacade studentFacade;
 
     public void execute(SendAuthCodeRequest request) {
 
         if (request.getAuthCodeType().equals(AuthCodeType.SIGN_UP)) {
-            authCodeFacade.sendSignUpAuthCode(request.getEmail());
+            sendSignUpAuthCode(request.getEmail());
         } else {
-            authCodeFacade.sendPasswordAuthCode(request.getEmail());
+            sendPasswordAuthCode(request.getEmail());
         }
+    }
+
+    private void sendSignUpAuthCode(String email) {
+
+        if (studentFacade.existsEmail(email)) {
+            throw StudentAlreadyExistsException.EXCEPTION;
+        }
+        authCodeFacade.sendMail(email);
+    }
+
+    private void sendPasswordAuthCode(String email) {
+
+        if (!studentFacade.existsEmail(email)) {
+            throw StudentNotFoundException.EXCEPTION;
+        }
+        authCodeFacade.sendMail(email);
     }
 }
