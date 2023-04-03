f05d3fcf8faffa3f36b0345cfef951dae4c64f35
Kangmin <ehfehf0629@gmail.com>
2023-04-03 17:24:15 +0900

    🦷 :: (#164)  SubCode 도메인 추가

---
+++ src/main/java/team/returm/jobis/domain/code/domain/repository/SubCodeJpaRepository.java
@@ -0,0 +1,7 @@
+package team.returm.jobis.domain.code.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.returm.jobis.domain.code.domain.SubCode;
+
+public interface SubCodeJpaRepository extends JpaRepository<SubCode, Long> {
+}
