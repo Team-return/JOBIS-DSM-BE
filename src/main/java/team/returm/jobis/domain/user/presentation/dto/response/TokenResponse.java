4562a51551dbfea1c2aaea528ee27f167463edf3
이하성 <tedsoftj1123@gmail.com>
2023-04-04 15:30:16 +0900

    🧩 :: (#172) refresh exp추가

--- src/main/java/team/returm/jobis/domain/user/presentation/dto/response/TokenResponse.java
+++ src/main/java/team/returm/jobis/domain/user/presentation/dto/response/TokenResponse.java
@@ -3,12 +3,14 @@ package team.returm.jobis.domain.user.presentation.dto.response;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
+import team.returm.jobis.domain.user.domain.enums.Authority;
 
 @Getter
 @Builder
 public class TokenResponse {
-
-    private String accessToken;
-    private String refreshToken;
-    private LocalDateTime accessExpiredAt;
+    private final String accessToken;
+    private final LocalDateTime accessExpiresAt;
+    private final String refreshToken;
+    private final LocalDateTime refreshExpiresAt;
+    private final Authority authority;
 }
