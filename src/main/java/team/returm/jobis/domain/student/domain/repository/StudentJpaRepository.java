d43bba842d38dfa9d096238106b36c9c24de164a
이하성 <tedsoftj1123@gmail.com>
2023-04-06 17:36:19 +0900

    🧩 :: 학생 중복체크

--- src/main/java/team/returm/jobis/domain/student/domain/repository/StudentJpaRepository.java
+++ src/main/java/team/returm/jobis/domain/student/domain/repository/StudentJpaRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.returm.jobis.domain.student.domain.Student;
 
 public interface StudentJpaRepository extends JpaRepository<Student, Long> {
+    boolean existsByGradeAndClassRoomAndNumber(Integer grade, Integer classRoom, Integer number);
 }
