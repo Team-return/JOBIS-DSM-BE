5fcfc8cadf94bc67dfcd64bb31c2322b9749f5b3
이하성 <tedsoftj1123@gmail.com>
2023-04-06 17:36:04 +0900

    ⚒️ :: 인증코드 존재여부 로직 변경

--- src/main/java/team/returm/jobis/domain/student/service/UpdateStudentPasswordService.java
+++ src/main/java/team/returm/jobis/domain/student/service/UpdateStudentPasswordService.java
@@ -2,7 +2,9 @@ package team.returm.jobis.domain.student.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import team.returm.jobis.domain.auth.facade.AuthCodeFacade;
+import team.returm.jobis.domain.auth.domain.AuthCode;
+import team.returm.jobis.domain.auth.domain.repository.AuthCodeRepository;
+import team.returm.jobis.domain.student.exception.UnverifiedEmailException;
 import team.returm.jobis.domain.student.presentation.dto.request.UpdatePasswordRequest;
 import team.returm.jobis.domain.user.domain.User;
 import team.returm.jobis.domain.user.domain.repository.UserRepository;
@@ -13,7 +15,7 @@ import team.returm.jobis.global.annotation.Service;
 @Service
 public class UpdateStudentPasswordService {
 
-    private final AuthCodeFacade authCodeFacade;
+    private final AuthCodeRepository authCodeRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -22,7 +24,10 @@ public class UpdateStudentPasswordService {
         if (!userRepository.existsByAccountId(request.getEmail())) {
             throw UserNotFoundException.EXCEPTION;
         }
-        authCodeFacade.checkIsVerified(request.getEmail());
+
+        AuthCode authCode = authCodeRepository.findById(request.getEmail())
+                .orElseThrow(() -> UnverifiedEmailException.EXCEPTION);
+        authCode.checkIsVerified();
 
         User user = userRepository.queryUserByAccountId(request.getEmail())
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
