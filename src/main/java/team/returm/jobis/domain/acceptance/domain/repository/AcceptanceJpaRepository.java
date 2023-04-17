fd227c3e639cb618e3882a22a0ff4444468d89e0
Kangmin <ehfehf0629@gmail.com>
2023-04-17 22:42:36 +0900

    🧩 :: (#202) 근로계약생 쿼리 작성

--- src/main/java/team/returm/jobis/domain/acceptance/domain/repository/AcceptanceJpaRepository.java
+++ src/main/java/team/returm/jobis/domain/acceptance/domain/repository/AcceptanceJpaRepository.java
@@ -3,5 +3,8 @@ package team.returm.jobis.domain.acceptance.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.returm.jobis.domain.acceptance.domain.Acceptance;
 
+import java.util.List;
+
 public interface AcceptanceJpaRepository extends JpaRepository<Acceptance, Long> {
+    List<Acceptance> queryByCompanyId(Long companyId);
 }
--- src/main/java/team/returm/jobis/domain/acceptance/domain/repository/AcceptanceRepository.java
+++ src/main/java/team/returm/jobis/domain/acceptance/domain/repository/AcceptanceRepository.java
@@ -3,6 +3,9 @@ package team.returm.jobis.domain.acceptance.domain.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import team.returm.jobis.domain.acceptance.domain.Acceptance;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -10,4 +13,8 @@ public class AcceptanceRepository {
 
     private final AcceptanceJpaRepository acceptanceJpaRepository;
     private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Acceptance> queryAcceptancesByCompanyId(Long companyId) {
+        return acceptanceJpaRepository.queryByCompanyId(companyId);
+    }
 }
