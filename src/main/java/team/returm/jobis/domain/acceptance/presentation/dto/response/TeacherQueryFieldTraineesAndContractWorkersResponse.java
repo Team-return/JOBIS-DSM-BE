6af0e391a6d2156b9b0311964996640ec0e57b28
Kangmin <ehfehf0629@gmail.com>
2023-04-18 16:15:16 +0900

    🛠 :: (#202) import 제거

--- src/main/java/team/returm/jobis/domain/acceptance/presentation/dto/response/TeacherQueryFieldTraineesAndContractWorkersResponse.java
+++ src/main/java/team/returm/jobis/domain/acceptance/presentation/dto/response/TeacherQueryFieldTraineesAndContractWorkersResponse.java
@@ -3,7 +3,6 @@ package team.returm.jobis.domain.acceptance.presentation.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import team.returm.jobis.domain.application.domain.repository.vo.QueryFieldTraineesVO;
 
 import java.time.LocalDate;
 import java.util.List;
