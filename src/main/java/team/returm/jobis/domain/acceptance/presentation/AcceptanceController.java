e412d8c8fd7905410cadf698cdb8405c72d35d4f
Kangmin <ehfehf0629@gmail.com>
2023-04-19 23:25:25 +0900

    🧩 :: (#213) 근로계약일자 변경 api

--- src/main/java/team/returm/jobis/domain/acceptance/presentation/AcceptanceController.java
+++ src/main/java/team/returm/jobis/domain/acceptance/presentation/AcceptanceController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import team.returm.jobis.domain.acceptance.presentation.dto.request.ChangeContractDateRequest;
 import team.returm.jobis.domain.acceptance.presentation.dto.response.TeacherQueryFieldTraineesAndContractWorkersResponse;
+import team.returm.jobis.domain.acceptance.service.ChangeContractDateService;
 import team.returm.jobis.domain.acceptance.service.TeacherQueryFieldTraineesAndContractWorkersService;
 import team.returm.jobis.domain.acceptance.presentation.dto.request.RegisterFieldTraineeRequest;
 import team.returm.jobis.domain.acceptance.service.RegisterFieldTraineeService;
@@ -21,6 +23,7 @@ import team.returm.jobis.domain.acceptance.service.RegisterFieldTraineeService;
 public class AcceptanceController {
 
     private final TeacherQueryFieldTraineesAndContractWorkersService teacherQueryFieldTraineesAndContractWorkersService;
+    private final ChangeContractDateService changeContractDateService;
     private final RegisterFieldTraineeService registerFieldTraineeService;
 
     @GetMapping("/{company-id}")
@@ -37,5 +40,11 @@ public class AcceptanceController {
             @RequestBody @Valid RegisterFieldTraineeRequest request
     ) {
         registerFieldTraineeService.execute(applicationId, request.getStartDate(), request.getEndDate());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/contract-date")
+    public void changeWorkContractDate(@RequestBody @Valid ChangeContractDateRequest request) {
+        changeContractDateService.execute(request);
     }
 }
