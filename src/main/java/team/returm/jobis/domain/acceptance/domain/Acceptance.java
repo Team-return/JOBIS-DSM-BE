11770dc7899dcc25bcfc664ee55e1fe4f068d511
Kangmin <ehfehf0629@gmail.com>
2023-05-01 19:16:24 +0900

    🧩 :: (#220) acceptance 생성자 접근제한자 변경

--- src/main/java/team/returm/jobis/domain/acceptance/domain/Acceptance.java
+++ src/main/java/team/returm/jobis/domain/acceptance/domain/Acceptance.java
@@ -1,5 +1,6 @@
 package team.returm.jobis.domain.acceptance.domain;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,7 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Acceptance {
 
