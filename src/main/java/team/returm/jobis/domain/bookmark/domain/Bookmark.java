008343ecd77c8fb85167fd0544ea76cb921f37ad
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-01 23:40:30 +0900

    🗂️ :: (#221) 북마크 엔티티

---
+++ src/main/java/team/returm/jobis/domain/bookmark/domain/Bookmark.java
@@ -0,0 +1,36 @@
+package team.returm.jobis.domain.bookmark.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+import team.returm.jobis.domain.recruitment.domain.Recruitment;
+import team.returm.jobis.domain.student.domain.Student;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@BatchSize(size = 200)
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@IdClass(BookMarkId.class)
+@Entity
+public class Bookmark {
+
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruitment_id", nullable = false)
+    private Recruitment recruitment;
+
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+}
