d3e7bff6f5fe69116ab64f57bde8dd2b2cd4f0a5
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-02 19:02:07 +0900

    🛠️ :: (#230) lint

--- src/main/java/team/returm/jobis/domain/student/domain/repository/StudentRepository.java
+++ src/main/java/team/returm/jobis/domain/student/domain/repository/StudentRepository.java
@@ -1,13 +1,15 @@
 package team.returm.jobis.domain.student.domain.repository;
 
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import team.returm.jobis.domain.student.domain.Student;
 
-@Repository
 @RequiredArgsConstructor
+@Repository
 public class StudentRepository {
+
     private final StudentJpaRepository studentJpaRepository;
 
     public Optional<Student> queryStudentById(Long studentId) {
