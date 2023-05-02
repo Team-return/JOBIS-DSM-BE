f00b81a1a7f88b5aed74ca1c14245873e2bf2f5d
Kangmin <ehfehf0629@gmail.com>
2023-05-02 23:21:59 +0900

    🧩 :: (#223) 공백 제거

--- src/main/java/team/returm/jobis/domain/student/domain/VerifiedStudent.java
+++ src/main/java/team/returm/jobis/domain/student/domain/VerifiedStudent.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
