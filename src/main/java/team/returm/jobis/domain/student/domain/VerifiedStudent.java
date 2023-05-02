6f327e4d6d1345ed27d9d2807f6e33c8d5a1d821
Kangmin <ehfehf0629@gmail.com>
2023-05-02 11:01:40 +0900

    🧩 :: (#223) 유니크 조건 추가

--- src/main/java/team/returm/jobis/domain/student/domain/VerifiedStudent.java
+++ src/main/java/team/returm/jobis/domain/student/domain/VerifiedStudent.java
@@ -2,23 +2,31 @@ package team.returm.jobis.domain.student.domain;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"studentGcn", "studentName"}
+                )
+        }
+)
 @Entity
-@Builder
 public class VerifiedStudent {
 
     @Id
+    @Column(columnDefinition = "CHAR(4)")
     private String studentGcn;
 
     @NotNull
