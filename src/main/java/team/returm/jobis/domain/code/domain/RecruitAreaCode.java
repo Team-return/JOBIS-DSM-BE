679edae712f44e3d0089f7b4beb2028e294e2ae8
이하성 <tedsoftj1123@gmail.com>
2023-05-07 01:06:08 +0900

    ⚒️ :: (#175) aaaa

--- src/main/java/team/returm/jobis/domain/code/domain/RecruitAreaCode.java
+++ src/main/java/team/returm/jobis/domain/code/domain/RecruitAreaCode.java
@@ -6,8 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.returm.jobis.domain.recruitment.domain.RecruitArea;
 
-import javax.persistence.*;
-
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @AllArgsConstructor
@@ -15,6 +19,7 @@ import javax.persistence.*;
 @Entity
 @IdClass(RecruitAreaCodeId.class)
 public class RecruitAreaCode {
+
     @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recruit_area_id", nullable = false)
