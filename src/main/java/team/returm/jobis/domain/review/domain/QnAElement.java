d7a4aa4adee2426fd2b4f2620fa7a198a8fbacc3
Kangmin <ehfehf0629@gmail.com>
2023-05-08 23:06:35 +0900

    🧩 :: (#233) QnAElement 추가

---
+++ src/main/java/team/returm/jobis/domain/review/domain/QnAElement.java
@@ -0,0 +1,16 @@
+package team.returm.jobis.domain.review.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QnAElement {
+
+    private String question;
+
+    private String answer;
+
+}
