9058f6e4fdf4cfcb15f15d682ac05ebb563b45b6
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-08 09:28:31 +0900

    🗂️ :: (#237) 북마크 조회 query

--- src/main/java/team/returm/jobis/domain/bookmark/domain/repository/BookmarkRepository.java
+++ src/main/java/team/returm/jobis/domain/bookmark/domain/repository/BookmarkRepository.java
@@ -1,13 +1,30 @@
 package team.returm.jobis.domain.bookmark.domain.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
+import team.returm.jobis.domain.bookmark.domain.Bookmark;
+import team.returm.jobis.domain.bookmark.domain.repository.vo.QQueryStudentBookmarksVO;
+import team.returm.jobis.domain.bookmark.domain.repository.vo.QueryStudentBookmarksVO;
+import team.returm.jobis.domain.recruitment.domain.Recruitment;
+import team.returm.jobis.domain.student.domain.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static team.returm.jobis.domain.bookmark.domain.QBookmark.bookmark;
+import static team.returm.jobis.domain.company.domain.QCompany.company;
+import static team.returm.jobis.domain.recruitment.domain.QRecruitment.recruitment;
+import static team.returm.jobis.domain.student.domain.QStudent.student;
 
 @RequiredArgsConstructor
 @Repository
 public class BookmarkRepository {
 
     private final BookmarkJpaRepository bookmarkJpaRepository;
+    private final JPAQueryFactory queryFactory;
 
     public void saveBookmark(Bookmark bookmark) {
         bookmarkJpaRepository.save(bookmark);
@@ -15,5 +32,29 @@ public class BookmarkRepository {
 
     public Optional<Bookmark> queryBookmarkByRecruitmentAndStudent(Recruitment recruitment, Student student) {
         return bookmarkJpaRepository.findByRecruitmentAndStudent(recruitment, student);
+    }
+
+    public void deleteBookmark(Bookmark bookmark) {
+        bookmarkJpaRepository.delete(bookmark);
+    }
+
+    public List<QueryStudentBookmarksVO> queryBookmarksByStudentId(Long studentId) {
+        return queryFactory
+                .selectFrom(bookmark)
+                .join(bookmark.recruitment, recruitment)
+                .join(recruitment.company, company)
+                .join(bookmark.student, student)
+                .where(student.id.eq(studentId))
+                .orderBy(bookmark.createdAt.desc())
+                .transform(
+                        groupBy(recruitment.id)
+                                .list(
+                                        new QQueryStudentBookmarksVO(
+                                                company.name,
+                                                recruitment.id,
+                                                bookmark.createdAt
+                                        )
+                                )
+                );
     }
 }
