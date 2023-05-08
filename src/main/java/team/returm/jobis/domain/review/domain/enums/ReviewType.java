c4ebfa20158edc8b404ed9d3f3b3a2156e17f2b6
Kangmin <ehfehf0629@gmail.com>
2023-05-08 23:06:19 +0900

    🧩 :: (#233) 기존 review 제거

--- src/main/java/team/returm/jobis/domain/review/domain/enums/ReviewType.java
+++
@@ -1,6 +0,0 @@
-package team.returm.jobis.domain.review.domain.enums;
-
-public enum ReviewType {
-    FIELD_TRAIN_REVIEW,
-    INTERVIEW_REVIEW
-}
--- src/main/java/team/returm/jobis/domain/review/domain/repository/ReviewJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package team.returm.jobis.domain.review.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import team.returm.jobis.domain.review.domain.Review;
-
-public interface ReviewJpaRepository extends CrudRepository<Review, Long> {
-}
