1c0a370639ebfb33a4168d5904d3a8840d0536bc
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-08 09:27:52 +0900

    🧩 :: (#237) 북마크 삭제 service

---
+++ src/main/java/team/returm/jobis/domain/bookmark/service/DeleteBookmarkService.java
@@ -0,0 +1,31 @@
+package team.returm.jobis.domain.bookmark.service;
+
+import lombok.RequiredArgsConstructor;
+import team.returm.jobis.domain.bookmark.domain.Bookmark;
+import team.returm.jobis.domain.bookmark.domain.repository.BookmarkRepository;
+import team.returm.jobis.domain.bookmark.exception.BookmarkNotFoundException;
+import team.returm.jobis.domain.recruitment.domain.Recruitment;
+import team.returm.jobis.domain.recruitment.domain.repository.RecruitmentRepository;
+import team.returm.jobis.domain.recruitment.exception.RecruitmentNotFoundException;
+import team.returm.jobis.domain.student.domain.Student;
+import team.returm.jobis.domain.user.facade.UserFacade;
+import team.returm.jobis.global.annotation.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteBookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final RecruitmentRepository recruitmentRepository;
+    private final UserFacade userFacade;
+
+    public void execute(Long recruitmentId) {
+        Student student = userFacade.getCurrentStudent();
+        Recruitment recruitment = recruitmentRepository.queryRecruitmentById(recruitmentId)
+                .orElseThrow(() -> RecruitmentNotFoundException.EXCEPTION);
+        Bookmark bookmark = bookmarkRepository.queryBookmarkByRecruitmentAndStudent(recruitment, student)
+                .orElseThrow(() -> BookmarkNotFoundException.EXCEPTION);
+
+        bookmarkRepository.deleteBookmark(bookmark);
+    }
+}
