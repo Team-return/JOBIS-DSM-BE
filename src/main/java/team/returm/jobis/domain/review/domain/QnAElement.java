11d42ce75c0a12cceacbbbcc837797ee26dc549b
Kangmin <ehfehf0629@gmail.com>
2023-05-09 16:44:26 +0900

    🧩 :: (#233) code 추가

--- src/main/java/team/returm/jobis/domain/review/domain/QnAElement.java
+++ src/main/java/team/returm/jobis/domain/review/domain/QnAElement.java
@@ -3,6 +3,7 @@ package team.returm.jobis.domain.review.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.returm.jobis.domain.code.domain.Code;
 
 @Getter
 @NoArgsConstructor
@@ -12,5 +13,7 @@ public class QnAElement {
     private String question;
 
     private String answer;
+
+    private Code code;
 
 }
