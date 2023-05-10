12b0883a7ee24b4fda3b4fcb9a1784f97e876e8a
Kangmin <ehfehf0629@gmail.com>
2023-05-10 16:16:30 +0900

    🧩 :: (#243) CreateReviewService

---
+++ src/main/java/team/returm/jobis/domain/review/service/CreateReviewService.java
@@ -0,0 +1,43 @@
+package team.returm.jobis.domain.review.service;
+
+import lombok.RequiredArgsConstructor;
+import team.returm.jobis.domain.application.domain.Application;
+import team.returm.jobis.domain.application.domain.enums.ApplicationStatus;
+import team.returm.jobis.domain.application.domain.repository.ApplicationRepository;
+import team.returm.jobis.domain.application.exception.ApplicationNotFoundException;
+import team.returm.jobis.domain.review.domain.Review;
+import team.returm.jobis.domain.review.domain.repository.ReviewRepository;
+import team.returm.jobis.domain.review.exception.ReviewCannotWriteException;
+import team.returm.jobis.domain.review.presentation.dto.CreateReviewRequest;
+import team.returm.jobis.domain.user.facade.UserFacade;
+import team.returm.jobis.global.annotation.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CreateReviewService {
+
+    private final UserFacade userFacade;
+    private final ApplicationRepository applicationRepository;
+    private final ReviewRepository reviewRepository;
+
+    public void execute(CreateReviewRequest request) {
+
+        Long currentUserId = userFacade.getCurrentUserId();
+
+        Application application = applicationRepository.queryApplicationByStudentId(currentUserId)
+                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+
+        if (application.getApplicationStatus() == ApplicationStatus.REQUESTED) {
+            throw ReviewCannotWriteException.EXCEPTION;
+        }
+
+        System.out.println(request.getQnaElements());
+        reviewRepository.save(Review.builder()
+                .companyId(request.getCompanyId())
+                .qnAElements(request.getQnaElements())
+                .companyName(request.getCompanyName())
+                .studentName(request.getStudentName())
+                .year(request.getYear())
+                .build());
+    }
+}
