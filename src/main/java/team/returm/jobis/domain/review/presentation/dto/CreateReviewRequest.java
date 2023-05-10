fb620ba49adfe06341d83029059d562323b04907
Kangmin <ehfehf0629@gmail.com>
2023-05-10 16:14:26 +0900

    🧩 :: (#243) CreateReviewRequest

---
+++ src/main/java/team/returm/jobis/domain/review/presentation/dto/CreateReviewRequest.java
@@ -0,0 +1,31 @@
+package team.returm.jobis.domain.review.presentation.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.returm.jobis.domain.review.domain.QnAElement;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class CreateReviewRequest {
+
+    @NotNull
+    private Long companyId;
+
+    @NotNull
+    private List<QnAElement> qnaElements;
+
+    @NotBlank
+    private String companyName;
+
+    @NotBlank
+    private String studentName;
+
+    @NotNull
+    private int year;
+
+}
