6062b0c2d3df927c7e28efa5d6a79454af9ed260
Kangmin <ehfehf0629@gmail.com>
2023-05-10 21:56:27 +0900

    🧩 :: (#246) QueryReviewsResponse

---
+++ src/main/java/team/returm/jobis/domain/review/presentation/dto/QueryReviewsResponse.java
@@ -0,0 +1,25 @@
+package team.returm.jobis.domain.review.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryReviewsResponse {
+
+    private final List<ReviewElement> reviewsResponse;
+
+
+    @Getter
+    @Builder
+    public static class ReviewElement {
+        private final String reviewId;
+        private final int year;
+        private final String writer;
+        private final LocalDate createdDate;
+    }
+}
