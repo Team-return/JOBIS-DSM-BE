61aab345f6b9245da8745faafc0271a0c5d62eef
Kangmin <ehfehf0629@gmail.com>
2023-05-10 16:23:25 +0900

    🧩 :: (#243) 후기 삭제 api

--- src/main/java/team/returm/jobis/domain/review/presentation/ReviewController.java
+++ src/main/java/team/returm/jobis/domain/review/presentation/ReviewController.java
@@ -2,6 +2,8 @@ package team.returm.jobis.domain.review.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team.returm.jobis.domain.review.presentation.dto.CreateReviewRequest;
 import team.returm.jobis.domain.review.service.CreateReviewService;
+import team.returm.jobis.domain.review.service.DeleteReviewService;
 
 import javax.validation.Valid;
 
@@ -18,10 +21,17 @@ import javax.validation.Valid;
 public class ReviewController {
 
     private final CreateReviewService createReviewService;
+    private final DeleteReviewService deleteReviewService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void createReview(@RequestBody @Valid CreateReviewRequest request) {
         createReviewService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{review-id}")
+    public void deleteReview(@PathVariable(name = "review-id") String reviewId) {
+        deleteReviewService.execute(reviewId);
     }
 }
