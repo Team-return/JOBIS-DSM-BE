291c6ed00ae5383f0bfc12dad436b32323721ac9
Kangmin <ehfehf0629@gmail.com>
2023-05-10 17:19:28 +0900

    🧩 :: (#243) EnableMongoRepositories 어노테이션 추가

--- src/main/java/team/returm/jobis/global/config/MongoConfig.java
+++ src/main/java/team/returm/jobis/global/config/MongoConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "team.returm.jobis")
 @Configuration
 public class MongoConfig {
 
