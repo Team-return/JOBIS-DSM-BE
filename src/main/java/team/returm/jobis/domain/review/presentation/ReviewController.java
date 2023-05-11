174a024a669aacc8d89c8315b81b495cfaf8264b
Kangmin <ehfehf0629@gmail.com>
2023-05-12 08:11:35 +0900

    🧩 :: (#246) 후기 상세 조회 api

--- src/main/java/team/returm/jobis/domain/review/presentation/ReviewController.java
+++ src/main/java/team/returm/jobis/domain/review/presentation/ReviewController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team.returm.jobis.domain.review.presentation.dto.QueryReviewDetailResponse;
 import team.returm.jobis.domain.review.presentation.dto.QueryReviewsResponse;
+import team.returm.jobis.domain.review.service.QueryReviewDetailService;
 import team.returm.jobis.domain.review.service.QueryReviewsService;
 
 @RequiredArgsConstructor
@@ -15,11 +17,19 @@ import team.returm.jobis.domain.review.service.QueryReviewsService;
 public class ReviewController {
 
     private final QueryReviewsService queryReviewsService;
+    private final QueryReviewDetailService queryReviewDetailService;
 
     @GetMapping("/{company-id}")
     public QueryReviewsResponse getReviews(
             @PathVariable(name = "company-id") Long companyId
     ) {
         return queryReviewsService.execute(companyId);
+    }
+
+    @GetMapping("/details/{review-id}")
+    public QueryReviewDetailResponse getReviewDetails(
+            @PathVariable(name = "review-id") String reviewId
+    ) {
+        return queryReviewDetailService.execute(reviewId);
     }
 }
