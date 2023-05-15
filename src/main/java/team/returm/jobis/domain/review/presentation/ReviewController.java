47a1011f1c30048478f1fa103c3067487cdc7db9
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-15 19:56:01 +0900

    ⚙️ :: (#246) conflicts 해결

--- src/main/java/team/returm/jobis/domain/review/presentation/ReviewController.java
+++ src/main/java/team/returm/jobis/domain/review/presentation/ReviewController.java
@@ -1,14 +1,24 @@
 package team.returm.jobis.domain.review.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import team.returm.jobis.domain.review.presentation.dto.CreateReviewRequest;
 import team.returm.jobis.domain.review.presentation.dto.QueryReviewDetailResponse;
 import team.returm.jobis.domain.review.presentation.dto.QueryReviewsResponse;
+import team.returm.jobis.domain.review.service.CreateReviewService;
+import team.returm.jobis.domain.review.service.DeleteReviewService;
 import team.returm.jobis.domain.review.service.QueryReviewDetailService;
 import team.returm.jobis.domain.review.service.QueryReviewsService;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/reviews")
@@ -17,6 +27,9 @@ public class ReviewController {
 
     private final QueryReviewsService queryReviewsService;
     private final QueryReviewDetailService queryReviewDetailService;
+    private final CreateReviewService createReviewService;
+    private final DeleteReviewService deleteReviewService;
+
 
     @GetMapping("/{company-id}")
     public QueryReviewsResponse getReviews(
@@ -31,42 +44,22 @@ public class ReviewController {
     ) {
         return queryReviewDetailService.execute(reviewId);
     }
-}
-
-package team.returm.jobis.domain.review.presentation;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import team.returm.jobis.domain.review.presentation.dto.CreateReviewRequest;
-import team.returm.jobis.domain.review.service.CreateReviewService;
-import team.returm.jobis.domain.review.service.DeleteReviewService;
-
-import javax.validation.Valid;
-
-@RequiredArgsConstructor
-@RequestMapping("/reviews")
-@RestController
-public class ReviewController {
-
-    private final CreateReviewService createReviewService;
-    private final DeleteReviewService deleteReviewService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void createReview(@RequestBody @Valid CreateReviewRequest request) {
+    public void createReview(
+            @RequestBody @Valid CreateReviewRequest request
+    ) {
         createReviewService.execute(request);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{review-id}")
-    public void deleteReview(@PathVariable(name = "review-id") String reviewId) {
+    public void deleteReview(
+            @PathVariable(name = "review-id") String reviewId
+    ) {
         deleteReviewService.execute(reviewId);
     }
 }
+
+
