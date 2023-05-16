7e0acb0b6bae8a76576ef971d3cf70808fd10763
Kangmin <ehfehf0629@gmail.com>
2023-05-16 23:37:55 +0900

    🧩 :: (main) 예외처리

--- src/main/java/team/returm/jobis/domain/review/service/CreateReviewService.java
+++ src/main/java/team/returm/jobis/domain/review/service/CreateReviewService.java
@@ -4,6 +4,12 @@ import lombok.RequiredArgsConstructor;
 import team.returm.jobis.domain.application.domain.enums.ApplicationStatus;
 import team.returm.jobis.domain.application.domain.repository.ApplicationRepository;
 import team.returm.jobis.domain.application.exception.ApplicationNotFoundException;
+import team.returm.jobis.domain.code.domain.Code;
+import team.returm.jobis.domain.code.domain.repository.CodeRepository;
+import team.returm.jobis.domain.code.exception.CodeNotFoundException;
+import team.returm.jobis.domain.company.domain.repository.CompanyRepository;
+import team.returm.jobis.domain.company.exception.CompanyNotFoundException;
+import team.returm.jobis.domain.review.domain.QnAElement;
 import team.returm.jobis.domain.review.domain.Review;
 import team.returm.jobis.domain.review.domain.repository.ReviewRepository;
 import team.returm.jobis.domain.review.presentation.dto.CreateReviewRequest;
@@ -14,6 +20,7 @@ import team.returm.jobis.domain.user.facade.UserFacade;
 import team.returm.jobis.global.annotation.Service;
 
 import java.time.Year;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -23,10 +30,26 @@ public class CreateReviewService {
     private final StudentRepository studentRepository;
     private final ApplicationRepository applicationRepository;
     private final ReviewRepository reviewRepository;
+    private final CompanyRepository companyRepository;
+    private final CodeRepository codeRepository;
 
     public void execute(CreateReviewRequest request) {
 
         Long currentUserId = userFacade.getCurrentUserId();
+
+        if (!companyRepository.existsCompanyById(request.getCompanyId())) {
+            throw CompanyNotFoundException.EXCEPTION;
+        }
+
+        List<Long> codeIds = request.getQnaElements().stream()
+                .map(QnAElement::getCodeId)
+                .toList();
+
+        List<Code> codes = codeRepository.queryCodesByIdIn(codeIds);
+
+        if (codes.size() != codeIds.size()) {
+            throw CodeNotFoundException.EXCEPTION;
+        }
 
         Student student =  studentRepository.queryStudentById(currentUserId)
                 .orElseThrow(() -> StudentNotFoundException.EXCEPTION);
