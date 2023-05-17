8b8eb421739dc1484a21fed27948e958855ef6c5
tedsoftj1123 <tedsoftj1123@gmail.com>
2023-05-17 22:56:38 +0900

    ⚒️ :: (#268) api 재사용

--- src/main/java/team/returm/jobis/domain/recruitment/presentation/dto/response/QueryRecruitmentDetailResponse.java
+++ src/main/java/team/returm/jobis/domain/recruitment/presentation/dto/response/QueryRecruitmentDetailResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Getter
 @Builder
-public class StudentRecruitDetailsResponse {
+public class QueryRecruitmentDetailResponse {
 
     private Long companyId;
     private List<RecruitAreaResponse> areas;
--- src/main/java/team/returm/jobis/domain/recruitment/presentation/dto/response/RecruitAreaResponse.java
+++ src/main/java/team/returm/jobis/domain/recruitment/presentation/dto/response/RecruitAreaResponse.java
@@ -34,6 +34,6 @@ public class RecruitAreaResponse {
                                 )
                                 .job(recruitArea.getJobCodes())
                                 .build()
-        ).toList();
+                ).toList();
     }
 }
