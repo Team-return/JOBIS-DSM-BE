6a60c2d34ab988a9256cf345f89403fc4e37337b
tedsoftj1123 <tedsoftj1123@gmail.com>
2023-05-22 00:20:02 +0900

    ⚒️ :: (#225) filter 객체 사용

---
+++ src/main/java/team/returm/jobis/domain/recruitment/presentation/dto/RecruitmentFilter.java
@@ -0,0 +1,30 @@
+package team.returm.jobis.domain.recruitment.presentation.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import team.returm.jobis.domain.code.domain.Code;
+import team.returm.jobis.domain.recruitment.domain.enums.RecruitStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+public class RecruitmentFilter {
+
+    private final Integer year;
+
+    private final LocalDate startDate;
+
+    private final LocalDate endDate;
+
+    private final RecruitStatus status;
+
+    private final String companyName;
+
+    private final Integer page;
+
+    private final List<Code> codes;
+
+    private final Long studentId;
+}
