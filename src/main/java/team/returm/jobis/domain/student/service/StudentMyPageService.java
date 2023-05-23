420d466278e18dbd5c1351153787f8f293fdadb9
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-23 23:03:20 +0900

    🛠️ :: (#273) lint

--- src/main/java/team/returm/jobis/domain/student/service/StudentMyPageService.java
+++ src/main/java/team/returm/jobis/domain/student/service/StudentMyPageService.java
@@ -1,16 +1,10 @@
 package team.returm.jobis.domain.student.service;
 
 import lombok.RequiredArgsConstructor;
-import team.returm.jobis.domain.application.domain.repository.ApplicationRepository;
-import team.returm.jobis.domain.application.domain.repository.vo.QueryApplyCompaniesVO;
-import team.returm.jobis.domain.application.domain.repository.vo.QueryTotalApplicationCountVO;
 import team.returm.jobis.domain.student.domain.Student;
-import team.returm.jobis.domain.student.exception.ClassRoomNotFoundException;
 import team.returm.jobis.domain.student.presentation.dto.response.StudentMyPageResponse;
 import team.returm.jobis.domain.user.facade.UserFacade;
 import team.returm.jobis.global.annotation.ReadOnlyService;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @ReadOnlyService
