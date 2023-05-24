04e5707bffead54f56fcbcefab64c87bffccb40b
gilgeunwoo <kesksjxhx@gmail.com>
2023-05-24 22:49:09 +0900

    🗂️ :: (#276) ImageProperty

---
+++ src/main/java/team/returm/jobis/global/util/ImageProperty.java
@@ -0,0 +1,11 @@
+package team.returm.jobis.global.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ImageProperty {
+
+    public static final String DEFAULT_STUDENT_PROFILE_IMAGE = "'https://jobis-file.s3.ap-northeast-2.amazonaws.com/EXTENSION_FILE/default_profile.png'";
+
+    public static final String DEFAULT_COMPANY_LOGO_IMAGE = "'https://jobis-file.s3.ap-northeast-2.amazonaws.com/LOGO_IMAGE/companydefault.png'";
+}
