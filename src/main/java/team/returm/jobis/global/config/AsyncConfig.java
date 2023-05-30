ceab2ba0ff9621ac1c7c8661c6200e93e2d7b701
tedsoftj1123 <tedsoftj1123@gmail.com>
2023-05-30 14:50:08 +0900

    🗂️ :: (#292) async config

---
+++ src/main/java/team/returm/jobis/global/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package team.returm.jobis.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("async-task-");
+        executor.initialize();
+
+        return executor;
+    }
+}
