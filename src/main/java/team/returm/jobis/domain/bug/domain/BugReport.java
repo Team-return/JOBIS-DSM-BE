b5df02543c457ed39841cc4244403987ec46a8e2
gilgeunwoo <kesksjxhx@gmail.com>
2023-06-18 19:10:52 +0900

    🧩 :: (#324) BugReport

---
+++ src/main/java/team/returm/jobis/domain/bug/domain/BugReport.java
@@ -0,0 +1,47 @@
+package team.returm.jobis.domain.bug.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.returm.jobis.domain.bug.domain.enums.DevelopmentArea;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class BugReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(20)")
+    private String title;
+
+    @Column(columnDefinition = "VARCHAR(400)")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(7)")
+    private DevelopmentArea developmentArea;
+
+    @OneToMany(mappedBy = "bugReport", orphanRemoval = true)
+    private final List<BugAttachment> bugAttachments  = new ArrayList<>();
+
+    @Builder
+    public BugReport(String title, String content, DevelopmentArea developmentArea) {
+        this.title = title;
+        this.content = content;
+        this.developmentArea = developmentArea;
+    }
+}
