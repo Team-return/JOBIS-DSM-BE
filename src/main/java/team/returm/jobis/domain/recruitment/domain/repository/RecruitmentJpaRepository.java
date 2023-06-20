d4ae2363bc1ef9ef598602e6d415d9c7289ecee4
gilgeunwoo <kesksjxhx@gmail.com>
2023-06-20 16:14:49 +0900

    🗂️ :: (#332) existsRecruitmentByCompanyAndStatusNot

--- src/main/java/team/returm/jobis/domain/recruitment/domain/repository/RecruitmentJpaRepository.java
+++ src/main/java/team/returm/jobis/domain/recruitment/domain/repository/RecruitmentJpaRepository.java
@@ -1,10 +1,14 @@
 package team.returm.jobis.domain.recruitment.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.returm.jobis.domain.company.domain.Company;
 import team.returm.jobis.domain.recruitment.domain.Recruitment;
+import team.returm.jobis.domain.recruitment.domain.enums.RecruitStatus;
 
 import java.util.List;
 
 public interface RecruitmentJpaRepository extends JpaRepository<Recruitment, Long> {
     List<Recruitment> findByIdIn(List<Long> recruitmentIds);
+
+    boolean existsByCompanyAndStatusNot(Company company, RecruitStatus status);
 }
