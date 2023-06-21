2a957c6c0fe70f2616536eff54c259024defb07e
gilgeunwoo <kesksjxhx@gmail.com>
2023-06-21 23:43:52 +0900

    🧩 :: (#346) 버그 제보 api

---
+++ src/main/java/team/returm/jobis/domain/bug/presentation/BugController.java
@@ -0,0 +1,27 @@
+package team.returm.jobis.domain.bug.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import team.returm.jobis.domain.bug.presentation.dto.request.CreateBugReportRequest;
+import team.returm.jobis.domain.bug.service.CreateBugReportService;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/bugs")
+@RestController
+public class BugController {
+
+    private final CreateBugReportService createBugReportService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createBugReport(@RequestBody @Valid CreateBugReportRequest request) {
+        createBugReportService.execute(request);
+    }
+}
