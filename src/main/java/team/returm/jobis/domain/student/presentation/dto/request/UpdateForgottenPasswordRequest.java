5c6a2d2e8245ee9fec075022b85afdea9a42b9a4
Kangmin <ehfehf0629@gmail.com>
2023-06-21 20:07:23 +0900

    🧩 :: (#344) 클래스 이름 변경

---
+++ src/main/java/team/returm/jobis/domain/student/presentation/dto/request/UpdateForgottenPasswordRequest.java
@@ -0,0 +1,21 @@
+package team.returm.jobis.domain.student.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.returm.jobis.global.util.RegexProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+public class UpdateForgottenPasswordRequest {
+
+    @NotBlank
+    @Pattern(regexp = RegexProperty.EMAIL)
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = RegexProperty.PASSWORD)
+    private String password;
+}
