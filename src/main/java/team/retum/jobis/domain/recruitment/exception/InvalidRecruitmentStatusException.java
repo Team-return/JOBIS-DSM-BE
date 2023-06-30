37fd9240fef5b68f35e7fa761c7ed1fef6e307c6
이하성 <tedsoftj1123@gmail.com>
2023-06-30 13:51:47 +0900

    ⚒️ :: (#353) lint

--- src/main/java/team/retum/jobis/domain/recruitment/exception/InvalidRecruitmentStatusException.java
+++ src/main/java/team/retum/jobis/domain/recruitment/exception/InvalidRecruitmentStatusException.java
@@ -1,7 +1,6 @@
 package team.retum.jobis.domain.recruitment.exception;
 
 import team.retum.jobis.domain.recruitment.exception.error.RecruitmentErrorCode;
-import team.retum.jobis.global.error.ErrorProperty;
 import team.retum.jobis.global.error.exception.JobisException;
 
 public class InvalidRecruitmentStatusException extends JobisException {
