663d91855a22530178835e854cd03ecf6f055b3f
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-10 08:59:48 +0900

    🛠️ :: (#339) 안쓰는 함수

--- src/main/java/team/retum/jobis/domain/recruitment/domain/Recruitment.java
+++ src/main/java/team/retum/jobis/domain/recruitment/domain/Recruitment.java
@@ -164,10 +164,4 @@ public class Recruitment extends BaseTimeEntity {
             throw InvalidRecruitmentStatusException.EXCEPTION;
         }
     }
-
-    public void checkRecruitAreaCount() {
-        if (this.recruitAreas.size() <= 1) {
-            throw RecruitAreaCannotDeleteException.EXCEPTION;
-        }
-    }
 }
