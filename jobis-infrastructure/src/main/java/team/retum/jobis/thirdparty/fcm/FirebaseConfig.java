c12fe6cb83e2cb9ce0e67e306c181cadc1bc8536
gilgeunwoo <kesksjxhx@gmail.com>
2023-08-20 15:09:51 +0900

    🗂️ :: (#380) FirebaseConfig

---
+++ jobis-infrastructure/src/main/java/team/retum/jobis/thirdparty/fcm/FirebaseConfig.java
@@ -0,0 +1,31 @@
+package team.retum.jobis.thirdparty.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${fcm.path}")
+    private String path;
+
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream account = new FileInputStream(path);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(account))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
