a799e41f51cdea6ed90d652ddc42bf156672eb7c
gilgeunwoo <kesksjxhx@gmail.com>
2023-08-25 09:23:35 +0900

    🗂️ :: (#388) Notification

---
+++ jobis-application/src/main/java/team/retum/jobis/domain/notification/model/Topic.java
@@ -0,0 +1,11 @@
+package team.retum.jobis.domain.notification.model;
+
+public enum Topic {
+
+    RECRUITMENT_STATUS_CHANGED,
+    NEW_APPLICATION,
+    APPLICATION_STATUS_CHANGED,
+    INTERVIEW_SOON,
+    NEW_RECRUITMENT,
+    RECRUITMENT_DONE,
+}
