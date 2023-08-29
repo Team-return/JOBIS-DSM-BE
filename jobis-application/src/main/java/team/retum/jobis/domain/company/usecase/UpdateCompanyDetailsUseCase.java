d8bfeb77fcabada1cc46ee99c0bdbd9725d02593
alsdl0629 <ehfehf0629@gmail.com>
2023-08-29 17:38:51 +0900

    🧩 :: (#401) 본인 회사 검증

--- jobis-application/src/main/java/team/retum/jobis/domain/company/usecase/UpdateCompanyDetailsUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/company/usecase/UpdateCompanyDetailsUseCase.java
@@ -2,6 +2,8 @@ package team.retum.jobis.domain.company.usecase;
 
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.UseCase;
+import team.retum.jobis.common.spi.SecurityPort;
+import team.retum.jobis.domain.auth.model.Authority;
 import team.retum.jobis.domain.company.dto.request.UpdateCompanyDetailsRequest;
 import team.retum.jobis.domain.company.exception.CompanyNotFoundException;
 import team.retum.jobis.domain.company.model.Company;
@@ -14,10 +16,15 @@ public class UpdateCompanyDetailsUseCase {
 
     private final QueryCompanyPort queryCompanyPort;
     private final CommandCompanyPort commandCompanyPort;
+    private final SecurityPort securityPort;
 
     public void execute(UpdateCompanyDetailsRequest request, Long companyId) {
         Company company = queryCompanyPort.queryCompanyById(companyId)
                 .orElseThrow(() -> CompanyNotFoundException.EXCEPTION);
+
+        if (securityPort.getCurrentUserAuthority() == Authority.COMPANY) {
+            company.verifySameCompany(securityPort.getCurrentCompany());
+        }
 
         commandCompanyPort.saveCompany(
                 company.update(
