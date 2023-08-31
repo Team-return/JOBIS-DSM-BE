df617f5f89131363625a8362b06d16af3c9c6506
alsdl0629 <ehfehf0629@gmail.com>
2023-08-31 11:58:52 +0900

    🧩 :: (#403) SlackConfig

---
+++ jobis-infrastructure/src/main/java/team/retum/jobis/thirdparty/slack/SlackConfig.java
@@ -0,0 +1,21 @@
+package team.retum.jobis.thirdparty.slack;
+
+import net.gpedro.integrations.slack.SlackApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SlackConfig {
+
+    @Value("${slack.url}")
+    private String url;
+
+    @Value("${slack.token}")
+    private String token;
+
+    @Bean
+    public SlackApi slackApi() {
+        return new SlackApi(url + token);
+    }
+}
