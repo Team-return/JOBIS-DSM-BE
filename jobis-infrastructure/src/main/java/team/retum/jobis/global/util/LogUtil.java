9492ba0af812021843022d9c9880d74a8bb9ee8a
이하성 <tedsoftj1123@gmail.com>
2023-09-21 19:28:41 +0900

    ⚒️ :: (#422) trace

---
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/util/LogUtil.java
@@ -0,0 +1,14 @@
+package team.retum.jobis.global.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class LogUtil {
+    public static String stackTraceToString(Exception exception) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        exception.printStackTrace(printWriter);
+
+        return stringWriter.toString();
+    }
+}
