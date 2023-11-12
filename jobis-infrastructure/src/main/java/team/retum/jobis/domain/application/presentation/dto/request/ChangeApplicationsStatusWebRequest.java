cef35c793b055a86480bb81bf3a20e6711af3640
이하성 <tedsoftj1123@gmail.com>
2023-11-12 23:47:59 +0900

    🦷 :: (#445) remove unused import

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/application/presentation/dto/request/ChangeApplicationsStatusWebRequest.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/application/presentation/dto/request/ChangeApplicationsStatusWebRequest.java
@@ -3,7 +3,6 @@ package team.retum.jobis.domain.application.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.retum.jobis.domain.application.model.ApplicationStatus;
-import team.retum.jobis.global.annotation.ValidListElements;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
