706074c987407e7a794bf27f392c074dfe2fb129
Kangmin <ehfehf0629@gmail.com>
2023-11-15 23:13:50 +0900

    ⚙️ :: (#449) jobcode 변환

--- jobis-application/src/main/java/team/retum/jobis/domain/recruitment/usecase/QueryMyRecruitmentUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/recruitment/usecase/QueryMyRecruitmentUseCase.java
@@ -3,6 +3,8 @@ package team.retum.jobis.domain.recruitment.usecase;
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.ReadOnlyUseCase;
 import team.retum.jobis.common.spi.SecurityPort;
+import team.retum.jobis.domain.code.model.Code;
+import team.retum.jobis.domain.code.spi.QueryCodePort;
 import team.retum.jobis.domain.recruitment.dto.response.QueryRecruitmentDetailResponse;
 import team.retum.jobis.domain.recruitment.dto.response.RecruitAreaResponse;
 import team.retum.jobis.domain.recruitment.exception.RecruitmentNotFoundException;
@@ -18,6 +20,7 @@ public class QueryMyRecruitmentUseCase {
 
     private final SecurityPort securityPort;
     private final QueryRecruitmentPort queryRecruitmentPort;
+    private final QueryCodePort queryCodePort;
 
     public QueryRecruitmentDetailResponse execute() {
         Long currentUserId = securityPort.getCurrentUserId();
@@ -25,10 +28,25 @@ public class QueryMyRecruitmentUseCase {
         Recruitment recruitment = queryRecruitmentPort.queryRecentRecruitmentByCompanyId(currentUserId)
                 .orElseThrow(() -> RecruitmentNotFoundException.EXCEPTION);
 
-
         RecruitmentDetailVO recruitmentDetail = queryRecruitmentPort.queryRecruitmentDetailById(recruitment.getId());
         List<RecruitAreaResponse> recruitAreaResponses =
-                queryRecruitmentPort.queryRecruitAreasByRecruitmentId(recruitment.getId());
+                queryRecruitmentPort.queryRecruitAreasByRecruitmentId(recruitment.getId())
+                        .stream().map(recruitAreaResponse -> RecruitAreaResponse.builder()
+                                    .id(recruitAreaResponse.getId())
+                                    .job(getJobKeywords(recruitAreaResponse.getJob()))
+                                    .tech(recruitAreaResponse.getTech())
+                                    .hiring(recruitAreaResponse.getHiring())
+                                    .majorTask(recruitAreaResponse.getMajorTask())
+                                    .preferentialTreatment(recruitAreaResponse.getPreferentialTreatment())
+                                    .build()
+                        ).toList();
+
         return QueryRecruitmentDetailResponse.of(recruitmentDetail, recruitAreaResponses);
+    }
+
+    private List<String> getJobKeywords(List<String> jobCodes) {
+        return queryCodePort.queryCodesByIdIn(
+                        jobCodes.stream().map(Long::parseLong).toList()
+                ).stream().map(Code::getKeyword).toList();
     }
 }
