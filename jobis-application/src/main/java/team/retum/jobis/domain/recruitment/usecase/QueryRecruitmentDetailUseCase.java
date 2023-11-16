e74e3e6a4906778f396a54daefaefb65bf996908
alsdl0629 <ehfehf0629@gmail.com>
2023-11-16 13:04:22 +0900

    🛠 :: (#449) 사용하지 않는 의존성 제거

--- jobis-application/src/main/java/team/retum/jobis/domain/recruitment/usecase/QueryRecruitmentDetailUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/recruitment/usecase/QueryRecruitmentDetailUseCase.java
@@ -2,17 +2,13 @@ package team.retum.jobis.domain.recruitment.usecase;
 
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.ReadOnlyUseCase;
-import team.retum.jobis.domain.code.service.GetKeywordsService;
 import team.retum.jobis.domain.recruitment.dto.response.QueryRecruitmentDetailResponse;
 import team.retum.jobis.domain.recruitment.service.GetRecruitmentDetailService;
-import team.retum.jobis.domain.recruitment.spi.QueryRecruitmentPort;
 
 @RequiredArgsConstructor
 @ReadOnlyUseCase
 public class QueryRecruitmentDetailUseCase {
 
-    private final QueryRecruitmentPort queryRecruitmentPort;
-    private final GetKeywordsService getKeywordsService;
     private final GetRecruitmentDetailService getRecruitmentDetailService;
 
     public QueryRecruitmentDetailResponse execute(Long recruitId) {
