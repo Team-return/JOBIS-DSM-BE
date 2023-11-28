33126df3a1e6ed8b205b2658f4d407bf44aa7d1e
이하성 <tedsoftj1123@gmail.com>
2023-11-28 20:20:53 +0900

    🦷 :: (#448) lint

--- jobis-application/src/main/java/team/retum/jobis/domain/recruitment/spi/vo/StudentRecruitmentVO.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/recruitment/spi/vo/StudentRecruitmentVO.java
@@ -2,10 +2,6 @@ package team.retum.jobis.domain.recruitment.spi.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import team.retum.jobis.domain.company.model.CompanyType;
-import team.retum.jobis.domain.recruitment.model.RecruitStatus;
-
-import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
