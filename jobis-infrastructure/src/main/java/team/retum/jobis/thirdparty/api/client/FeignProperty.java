d55fc7b3b2d0ff0fb5558e43286e4d706402fd9c
gilgeunwoo <kesksjxhx@gmail.com>
2023-12-04 16:18:30 +0900

    🗑️ :: (#481) constructorBinding 제거

--- jobis-infrastructure/src/main/java/team/retum/jobis/thirdparty/api/client/FeignProperty.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/thirdparty/api/client/FeignProperty.java
@@ -3,12 +3,10 @@ package team.retum.jobis.thirdparty.api.client;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 
 @Getter
 @RequiredArgsConstructor
-@ConstructorBinding
 @ConfigurationProperties(prefix = "openfeign")
 public class FeignProperty {
 
