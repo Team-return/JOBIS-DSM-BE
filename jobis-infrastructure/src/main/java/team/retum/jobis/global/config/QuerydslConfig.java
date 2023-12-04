009b3f0b4be3b4fac611c328dc23495d8160e93a
gilgeunwoo <kesksjxhx@gmail.com>
2023-12-04 22:57:00 +0900

    ⚙️ :: (#develop) 모집의뢰서 상세조회 500 해결

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/config/QuerydslConfig.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/config/QuerydslConfig.java
@@ -1,5 +1,6 @@
 package team.retum.jobis.global.config;
 
+import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -13,6 +14,6 @@ public class QuerydslConfig {
 
     @Bean
     public JPAQueryFactory queryFactory() {
-        return new JPAQueryFactory(entityManager);
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
     }
 }
