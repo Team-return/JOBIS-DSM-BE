f2e55d9c36c0ba23192aa17b09c39f2656cf6615
Kangmin <ehfehf0629@gmail.com>
2023-12-10 23:15:35 +0900

    🧩 :: (#493) CacheConfig

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RedisConfig.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package team.retum.jobis.global.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@RequiredArgsConstructor
 @EnableRedisRepositories
 @Configuration
 public class RedisConfig {
