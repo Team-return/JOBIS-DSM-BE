6e74b8faa8f84ab0470350816f02f5f165c0d446
gilgeunwoo <kesksjxhx@gmail.com>
2023-12-29 10:47:39 +0900

    🛩️ :: (#499) @Indexed 메모리 누수 해결

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RedisConfig.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RedisConfig.java
@@ -7,10 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @RequiredArgsConstructor
-@EnableRedisRepositories
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @Configuration
 public class RedisConfig {
     @Value("${redis.port}")
