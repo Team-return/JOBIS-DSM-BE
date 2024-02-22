94fa3cf4c42d756a8516f4dc8cc6ec574a28ef48
이하성 <tedsoftj1123@gmail.com>
2024-02-22 15:35:16 +0900

    🦷 :: (#522) file domain response to record

--- jobis-application/src/main/java/team/retum/jobis/domain/file/usecase/CreateFileUploadUrlUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/file/usecase/CreateFileUploadUrlUseCase.java
@@ -3,10 +3,12 @@ package team.retum.jobis.domain.file.usecase;
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.Service;
 import team.retum.jobis.common.util.FileUtil;
-import team.retum.jobis.domain.file.dto.CreateFileUploadUrlRequest;
+import team.retum.jobis.domain.file.dto.request.FileRequest;
 import team.retum.jobis.domain.file.dto.response.CreateFileUploadUrlResponse;
 import team.retum.jobis.domain.file.dto.response.CreateFileUploadUrlResponse.UrlResponse;
 import team.retum.jobis.domain.file.spi.FilePort;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,12 +16,12 @@ public class CreateFileUploadUrlUseCase {
 
     private final FilePort filePort;
 
-    public CreateFileUploadUrlResponse execute(CreateFileUploadUrlRequest request) {
+    public CreateFileUploadUrlResponse execute(List<FileRequest> fileRequests) {
         return new CreateFileUploadUrlResponse(
-                request.getFiles().stream()
+                fileRequests.stream()
                         .map(
                                 file -> {
-                                    String fullFileName = FileUtil.generateFullFileName(file.getType(), file.getFileName());
+                                    String fullFileName = FileUtil.generateFullFileName(file.type(), file.fileName());
                                     String url = filePort.generateFileUploadUrl(fullFileName);
                                     return new UrlResponse(
                                             fullFileName,
--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/file/presentation/FileWebAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/file/presentation/FileWebAdapter.java
@@ -6,64 +6,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import team.retum.jobis.domain.file.dto.request.FileRequest;
 import team.retum.jobis.domain.file.dto.response.CreateFileUploadUrlResponse;
-import team.retum.jobis.domain.file.dto.response.FileUploadResponse;
-import team.retum.jobis.domain.file.exception.FileNotFoundException;
-import team.retum.jobis.domain.file.exception.FileUploadFailedException;
-import team.retum.jobis.domain.file.model.FileType;
 import team.retum.jobis.domain.file.presentation.dto.CreatePreSignedUrlWebRequest;
 import team.retum.jobis.domain.file.usecase.CreateFileUploadUrlUseCase;
-import team.retum.jobis.domain.file.usecase.FileUploadUseCase;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.List;
 
 @RestController
 @RequestMapping("/files")
 @RequiredArgsConstructor
 public class FileWebAdapter {
-
-    private final FileUploadUseCase fileUploadUseCase;
     private final CreateFileUploadUrlUseCase createFileUploadUrlUseCase;
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public FileUploadResponse uploadFile(
-            @RequestParam("file") List<MultipartFile> multipartFiles,
-            @RequestParam("type") FileType fileType
-    ) {
-        return fileUploadUseCase.execute(
-                multipartFiles.stream()
-                        .map(multipartFile -> {
-                            if (multipartFile == null || multipartFile.getOriginalFilename() == null) {
-                                throw FileNotFoundException.EXCEPTION;
-                            }
-                            File file = new File(multipartFile.getOriginalFilename());
-
-                            try (OutputStream outputStream = new FileOutputStream(file)) {
-                                outputStream.write(multipartFile.getBytes());
-                            } catch (IOException e) {
-                                throw FileUploadFailedException.EXCEPTION;
-                            }
-
-                            return file;
-                        }).toList(),
-                fileType
-        );
-    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/pre-signed")
     public CreateFileUploadUrlResponse createPreSignedUrl(
             @RequestBody @Valid CreatePreSignedUrlWebRequest request
     ) {
-        return createFileUploadUrlUseCase.execute(request.toDomainRequest());
+        return createFileUploadUrlUseCase.execute(
+                request.getFiles().stream()
+                        .map(file -> new FileRequest(file.getType(), file.getFileName()))
+                        .toList()
+        );
     }
 }
