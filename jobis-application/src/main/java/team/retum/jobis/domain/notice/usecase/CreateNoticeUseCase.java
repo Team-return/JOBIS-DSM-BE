1a135ca9126eead49f4b262568ddee546a1f9c51
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-23 16:13:49 +0900

    🧩 :: (#526) 공지사항 생성 useCase

---
+++ jobis-application/src/main/java/team/retum/jobis/domain/notice/usecase/CreateNoticeUseCase.java
@@ -0,0 +1,33 @@
+package team.retum.jobis.domain.notice.usecase;
+
+import lombok.RequiredArgsConstructor;
+import team.retum.jobis.common.annotation.UseCase;
+import team.retum.jobis.domain.notice.dto.CreateNoticeRequest;
+import team.retum.jobis.domain.notice.model.Notice;
+import team.retum.jobis.domain.notice.model.NoticeAttachment;
+import team.retum.jobis.domain.notice.spi.CommandNoticePort;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@UseCase
+public class CreateNoticeUseCase {
+
+    private final CommandNoticePort commandNoticePort;
+
+    public void execute(CreateNoticeRequest request) {
+
+        List<NoticeAttachment> attachments = request.getAttachments()
+                .stream()
+                .map(attachment -> new NoticeAttachment(attachment.getUrl(), attachment.getType()))
+                .toList();
+
+        commandNoticePort.saveNotice(
+                Notice.builder()
+                        .title(request.getTitle())
+                        .content(request.getContent())
+                        .attachments(attachments)
+                        .build()
+        );
+    }
+}
