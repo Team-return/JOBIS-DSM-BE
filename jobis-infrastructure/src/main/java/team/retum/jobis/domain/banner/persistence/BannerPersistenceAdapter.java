f97dc0b807a12bc0f63fa5f3c7807a123b981043
ilyoil2 <0211ilyoil@gmail.com>
2024-02-24 18:50:13 +0900

    🧩 :: (#530) 배너 조회 메서드 추가

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/banner/persistence/BannerPersistenceAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/banner/persistence/BannerPersistenceAdapter.java
@@ -1,13 +1,20 @@
 package team.retum.jobis.domain.banner.persistence;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import team.retum.jobis.domain.banner.model.Banner;
 import team.retum.jobis.domain.banner.persistence.mapper.BannerMapper;
 import team.retum.jobis.domain.banner.persistence.repository.BannerJpaRepository;
+import team.retum.jobis.domain.banner.persistence.repository.vo.QQueryBannerVO;
 import team.retum.jobis.domain.banner.spi.BannerPort;
+import team.retum.jobis.domain.banner.spi.vo.BannerVO;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+
+import static team.retum.jobis.domain.bookmark.persistence.entity.QBannerEntity.bannerEntity;
 
 @RequiredArgsConstructor
 @Repository
@@ -15,6 +22,7 @@ public class BannerPersistenceAdapter implements BannerPort {
 
     private final BannerJpaRepository bannerJpaRepository;
     private final BannerMapper bannerMapper;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public void saveBanner(Banner banner) {
@@ -34,5 +42,23 @@ public class BannerPersistenceAdapter implements BannerPort {
     public Optional<Banner> queryBannerById(Long bannerId) {
         return bannerJpaRepository.findById(bannerId)
                 .map(bannerMapper::toDomain);
+    }
+
+    @Override
+    public List<BannerVO> queryCurrentBanners() {
+        return queryFactory
+                .select(
+                        new QQueryBannerVO(
+                                bannerEntity.id,
+                                bannerEntity.bannerUrl,
+                                bannerEntity.bannerType
+                        )
+                )
+                .from(bannerEntity)
+                .where(
+                        bannerEntity.startDate.before(LocalDateTime.now()),
+                        bannerEntity.endDate.after(LocalDateTime.now())
+                )
+                .fetch();
     }
 }
