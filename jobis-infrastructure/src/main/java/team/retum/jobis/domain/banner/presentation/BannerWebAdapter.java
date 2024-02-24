6e87a18e89fa776aae7fb56b4707419cd1bffa00
ilyoil2 <0211ilyoil@gmail.com>
2024-02-24 18:49:29 +0900

    🧩 :: (#530) 배너 조회 webAdapter & response

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/banner/presentation/BannerWebAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/banner/presentation/BannerWebAdapter.java
@@ -6,13 +6,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import team.retum.jobis.domain.banner.dto.QueryBannersResponse;
 import team.retum.jobis.domain.banner.presentation.dto.CreateBannerWebRequest;
 import team.retum.jobis.domain.banner.usecase.CreateBannerUseCase;
 import team.retum.jobis.domain.banner.usecase.DeleteBannerUseCase;
+import team.retum.jobis.domain.banner.usecase.QueryBannersUseCase;
 
 @RequiredArgsConstructor
 @RequestMapping("/banners")
@@ -21,6 +24,7 @@ public class BannerWebAdapter {
 
     private final CreateBannerUseCase createBannerUseCase;
     private final DeleteBannerUseCase deleteBannerUseCase;
+    private final QueryBannersUseCase queryBannersUseCase;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -34,5 +38,10 @@ public class BannerWebAdapter {
             @PathVariable("banner-id") Long bannerId
     ) {
         deleteBannerUseCase.execute(bannerId);
+    }
+
+    @GetMapping
+    public QueryBannersResponse queryBanners() {
+        return queryBannersUseCase.execute();
     }
 }
