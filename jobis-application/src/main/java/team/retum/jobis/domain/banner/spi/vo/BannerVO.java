b636bc8e39b1453fbf7ff7dd476f544e39fb4b59
ilyoil2 <0211ilyoil@gmail.com>
2024-02-24 18:49:29 +0900

    🧩 :: (#530) 배너 조회 webAdapter & response

---
+++ jobis-application/src/main/java/team/retum/jobis/domain/banner/spi/vo/BannerVO.java
@@ -0,0 +1,13 @@
+package team.retum.jobis.domain.banner.spi.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import team.retum.jobis.domain.banner.model.BannerType;
+
+@Getter
+@AllArgsConstructor
+public class BannerVO {
+    private final Long id;
+    private final String bannerUrl;
+    private final BannerType bannerType;
+}
