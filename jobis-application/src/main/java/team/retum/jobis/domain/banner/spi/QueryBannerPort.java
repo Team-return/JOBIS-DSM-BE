a46dd3f0a108727319c9bb051962951727aaf53a
ilyoil2 <0211ilyoil@gmail.com>
2024-02-24 18:50:13 +0900

    🧩 :: (#530) 배너 조회 메서드 추가

--- jobis-application/src/main/java/team/retum/jobis/domain/banner/spi/QueryBannerPort.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/banner/spi/QueryBannerPort.java
@@ -1,9 +1,13 @@
 package team.retum.jobis.domain.banner.spi;
 
 import team.retum.jobis.domain.banner.model.Banner;
+import team.retum.jobis.domain.banner.spi.vo.BannerVO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QueryBannerPort {
     Optional<Banner> queryBannerById(Long bannerId);
+
+    List<BannerVO> queryCurrentBanners();
 }
