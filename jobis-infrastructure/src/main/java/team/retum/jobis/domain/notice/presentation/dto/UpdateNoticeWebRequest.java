885218c5e6fd874dee3d98ae7216c53c5d5379b7
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-28 22:22:42 +0900

    🧩 :: (#536) 공지사항 수정 request, adapter

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/notice/presentation/dto/UpdateNoticeWebRequest.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/notice/presentation/dto/UpdateNoticeWebRequest.java
@@ -1,9 +1,9 @@
 package team.retum.jobis.domain.notice.presentation.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.retum.jobis.domain.notice.dto.request.UpdateNoticeRequest;
 
 @Getter
 @NoArgsConstructor
@@ -14,4 +14,11 @@ public class UpdateNoticeWebRequest {
 
     @NotBlank
     private String content;
+
+    public UpdateNoticeRequest toDomainRequest() {
+        return new UpdateNoticeRequest(
+                this.title,
+                this.content
+        );
+    }
 }
