6996361d6765aec6f2e731e040e2321df2d7c3e8
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-28 22:23:10 +0900

    🧩 :: (#536) 공지사항 수정 로직

---
+++ jobis-application/src/main/java/team/retum/jobis/domain/notice/usecase/UpdateNoticeUseCase.java
@@ -0,0 +1,29 @@
+package team.retum.jobis.domain.notice.usecase;
+
+import lombok.RequiredArgsConstructor;
+import team.retum.jobis.common.annotation.UseCase;
+import team.retum.jobis.domain.notice.dto.request.UpdateNoticeRequest;
+import team.retum.jobis.domain.notice.exception.NoticeNotFoundException;
+import team.retum.jobis.domain.notice.model.Notice;
+import team.retum.jobis.domain.notice.spi.CommandNoticePort;
+import team.retum.jobis.domain.notice.spi.QueryNoticePort;
+
+@RequiredArgsConstructor
+@UseCase
+public class UpdateNoticeUseCase {
+
+    private final CommandNoticePort commandNoticePort;
+    private final QueryNoticePort queryNoticePort;
+
+    public void execute(UpdateNoticeRequest request, Long noticeId) {
+        Notice notice = queryNoticePort.queryNoticeById(noticeId)
+                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+
+        commandNoticePort.saveNotice(
+                notice.update(
+                        request.getTitle(),
+                        request.getContent()
+                )
+        );
+    }
+}
