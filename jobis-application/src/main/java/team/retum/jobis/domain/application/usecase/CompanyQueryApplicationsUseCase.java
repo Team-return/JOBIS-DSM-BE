d89f22c8884bf5464f773a9eb409e9a90046f644
alsdl0629 <ehfehf0629@gmail.com>
2024-02-29 22:42:42 +0900

    🛠 :: (#543) code refactoring

--- jobis-application/src/main/java/team/retum/jobis/domain/application/usecase/CompanyQueryApplicationsUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/application/usecase/CompanyQueryApplicationsUseCase.java
@@ -3,16 +3,17 @@ package team.retum.jobis.domain.application.usecase;
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.ReadOnlyUseCase;
 import team.retum.jobis.common.spi.SecurityPort;
-import team.retum.jobis.domain.application.dto.response.AttachmentResponse;
 import team.retum.jobis.domain.application.dto.response.CompanyQueryApplicationsResponse;
-import team.retum.jobis.domain.application.dto.response.CompanyQueryApplicationsResponse.CompanyQueryApplicationResponse;
+import team.retum.jobis.domain.application.model.ApplicationFilter;
 import team.retum.jobis.domain.application.model.ApplicationStatus;
 import team.retum.jobis.domain.application.spi.QueryApplicationPort;
+import team.retum.jobis.domain.application.spi.vo.ApplicationVO;
 import team.retum.jobis.domain.company.model.Company;
 import team.retum.jobis.domain.recruitment.exception.RecruitmentNotFoundException;
 import team.retum.jobis.domain.recruitment.model.Recruitment;
 import team.retum.jobis.domain.recruitment.spi.QueryRecruitmentPort;
-import team.retum.jobis.domain.student.model.SchoolNumber;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @ReadOnlyUseCase
@@ -28,30 +29,13 @@ public class CompanyQueryApplicationsUseCase {
         Recruitment recruitment = queryRecruitmentPort.queryRecentRecruitmentByCompanyId(company.getId())
                 .orElseThrow(() -> RecruitmentNotFoundException.EXCEPTION);
 
-        return new CompanyQueryApplicationsResponse(
-                queryApplicationPort.queryApplicationByConditions(
-                                recruitment.getId(), null, ApplicationStatus.SEND, null
-                        ).stream()
-                        .map(application -> CompanyQueryApplicationResponse.builder()
-                                .applicationId(application.getId())
-                                .studentName(application.getName())
-                                .studentNumber(
-                                        SchoolNumber.processSchoolNumber(
-                                                SchoolNumber.builder()
-                                                        .grade(application.getGrade())
-                                                        .classRoom(application.getClassNumber())
-                                                        .number(application.getNumber())
-                                                        .build()
-                                        )
-                                )
-                                .profileImageUrl(application.getProfileImageUrl())
-                                .attachments(
-                                        application.getApplicationAttachmentEntities().stream()
-                                                .map(AttachmentResponse::of).toList()
-                                )
-                                .createdAt(application.getCreatedAt().toLocalDate())
-                                .build()
-                        ).toList()
-        );
+        ApplicationFilter applicationFilter = ApplicationFilter.builder()
+                .recruitmentId(recruitment.getId())
+                .applicationStatus(ApplicationStatus.SEND)
+                .build();
+
+        List<ApplicationVO> applicationVOs = queryApplicationPort.queryApplicationByConditions(applicationFilter);
+
+        return CompanyQueryApplicationsResponse.of(applicationVOs);
     }
 }
