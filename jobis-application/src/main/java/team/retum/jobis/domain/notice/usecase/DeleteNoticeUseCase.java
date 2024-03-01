b24324065bd1647cadd33d69224c4f964ce159e2
4mjeo <4mj.eo@dsm.hs.kr>
2024-03-01 18:08:27 +0900

    🧩 :: (#548) 공지사항 삭제 로직

---
+++ jobis-application/src/main/java/team/retum/jobis/domain/notice/usecase/DeleteNoticeUseCase.java
@@ -0,0 +1,24 @@
+package team.retum.jobis.domain.notice.usecase;
+
+import lombok.RequiredArgsConstructor;
+import team.retum.jobis.common.annotation.UseCase;
+import team.retum.jobis.domain.notice.exception.NoticeNotFoundException;
+import team.retum.jobis.domain.notice.model.Notice;
+import team.retum.jobis.domain.notice.spi.CommandNoticePort;
+import team.retum.jobis.domain.notice.spi.QueryNoticePort;
+
+@RequiredArgsConstructor
+@UseCase
+public class DeleteNoticeUseCase {
+
+    private final CommandNoticePort commandNoticePort;
+    private final QueryNoticePort queryNoticePort;
+
+    public void execute(Long noticeId) {
+
+        Notice notice = queryNoticePort.queryNoticeById(noticeId)
+                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+
+        commandNoticePort.deleteNotice(notice);
+    }
+}
