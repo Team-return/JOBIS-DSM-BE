893e4daf008a927102b068bb19d0944a4709d6c2
4mjeo <4mj.eo@dsm.hs.kr>
2024-03-05 23:12:10 +0900

    🧩 :: (#558) 권한 설정 변경

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/security/SecurityConfig.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/security/SecurityConfig.java
@@ -149,6 +149,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/notices").hasAuthority(TEACHER.name())
                                 .requestMatchers(HttpMethod.PATCH, "/notices/{notice-id}").hasAuthority(TEACHER.name())
                                 .requestMatchers(HttpMethod.DELETE, "/notices/{notice-id}").hasAuthority(TEACHER.name())
+                                .requestMatchers(HttpMethod.GET, "/notices").hasAnyAuthority(STUDENT.name(), TEACHER.name())
 
                                 // notification
                                 .requestMatchers(HttpMethod.GET, "/notifications").authenticated()
