2a44a3737ee29b6cd60782cedea8e716d7fd4f8b
ilyoil2 <0211ilyoil@gmail.com>
2024-03-09 16:05:09 +0900

    🧩 :: (#570) queryNoticeDetail api

---
+++ jobis-application/src/main/java/team/retum/jobis/domain/notice/dto/response/QueryNoticeDetailResponse.java
@@ -0,0 +1,28 @@
+package team.retum.jobis.domain.notice.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import team.retum.jobis.domain.notice.model.Notice;
+import team.retum.jobis.domain.notice.model.NoticeAttachment;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class QueryNoticeDetailResponse {
+
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final List<NoticeAttachment> attachments;
+
+    public static QueryNoticeDetailResponse of(Notice notice) {
+        return QueryNoticeDetailResponse.builder()
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .createdAt(notice.getCreatedAt())
+                .attachments(notice.getAttachments())
+                .build();
+    }
+}
---
+++ jobis-application/src/main/java/team/retum/jobis/domain/notice/usecase/QueryNoticeDetailUseCase.java
@@ -0,0 +1,25 @@
+package team.retum.jobis.domain.notice.usecase;
+
+
+import lombok.RequiredArgsConstructor;
+import team.retum.jobis.common.annotation.ReadOnlyUseCase;
+import team.retum.jobis.domain.notice.dto.response.QueryNoticeDetailResponse;
+import team.retum.jobis.domain.notice.exception.NoticeNotFoundException;
+import team.retum.jobis.domain.notice.model.Notice;
+import team.retum.jobis.domain.notice.spi.QueryNoticePort;
+
+@RequiredArgsConstructor
+@ReadOnlyUseCase
+public class QueryNoticeDetailUseCase {
+
+    private final QueryNoticePort queryNoticePort;
+
+    public QueryNoticeDetailResponse execute(Long noticeId) {
+
+        Notice notice = queryNoticePort.queryNoticeById(noticeId)
+                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+
+        return QueryNoticeDetailResponse.of(notice);
+
+    }
+}
