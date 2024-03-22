e11116df6b54e48ed3b3bfa46b5cb8936d1b190a
길근우 <kesksjxhx@gmail.com>
2024-03-22 20:42:56 +0900

    🛠️ :: (#602) 여러곳에서 사용되는 check service로 분리

--- jobis-application/src/main/java/team/retum/jobis/domain/recruitment/usecase/UpdateRecruitAreaUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/recruitment/usecase/UpdateRecruitAreaUseCase.java
@@ -5,7 +5,7 @@ import team.retum.jobis.common.annotation.UseCase;
 import team.retum.jobis.domain.recruitment.dto.request.CreateRecruitAreaRequest;
 import team.retum.jobis.domain.recruitment.exception.RecruitAreaNotFoundException;
 import team.retum.jobis.domain.recruitment.model.RecruitArea;
-import team.retum.jobis.domain.recruitment.service.CheckRecruitmentPermissionService;
+import team.retum.jobis.domain.recruitment.checker.RecruitmentChecker;
 import team.retum.jobis.domain.recruitment.spi.CommandRecruitmentPort;
 import team.retum.jobis.domain.recruitment.spi.QueryRecruitmentPort;
 
@@ -14,12 +14,13 @@ import team.retum.jobis.domain.recruitment.spi.QueryRecruitmentPort;
 public class UpdateRecruitAreaUseCase {
     private final CommandRecruitmentPort commandRecruitmentPort;
     private final QueryRecruitmentPort queryRecruitmentPort;
-    private final CheckRecruitmentPermissionService checkRecruitmentPermissionService;
+    private final RecruitmentChecker recruitmentChecker;
 
     public void execute(CreateRecruitAreaRequest request, Long recruitAreaId) {
         RecruitArea recruitArea = queryRecruitmentPort.queryRecruitmentAreaById(recruitAreaId)
                 .orElseThrow(() -> RecruitAreaNotFoundException.EXCEPTION);
-        checkRecruitmentPermissionService.checkPermission(recruitArea);
+
+        recruitmentChecker.checkPermission(recruitArea);
 
         commandRecruitmentPort.saveRecruitmentArea(recruitArea.update(request));
     }
--- jobis-application/src/main/java/team/retum/jobis/domain/user/usecase/LoginUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/user/usecase/LoginUseCase.java
@@ -5,6 +5,7 @@ import team.retum.jobis.common.annotation.UseCase;
 import team.retum.jobis.common.spi.SecurityPort;
 import team.retum.jobis.domain.auth.dto.response.TokenResponse;
 import team.retum.jobis.domain.auth.spi.JwtPort;
+import team.retum.jobis.domain.user.checker.UserChecker;
 import team.retum.jobis.domain.user.dto.request.LoginRequest;
 import team.retum.jobis.domain.user.exception.InvalidPasswordException;
 import team.retum.jobis.domain.user.exception.UserNotFoundException;
@@ -18,16 +19,14 @@ public class LoginUseCase {
 
     private final QueryUserPort queryUserPort;
     private final CommandUserPort commandUserPort;
-    private final SecurityPort securityPort;
+    private final UserChecker userChecker;
     private final JwtPort jwtPort;
 
     public TokenResponse execute(LoginRequest request) {
         User user = queryUserPort.queryUserByAccountId(request.accountId())
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-        if (!securityPort.isPasswordMatch(request.password(), user.getPassword())) {
-            throw InvalidPasswordException.EXCEPTION;
-        }
+        userChecker.checkPasswordMatch(request.password(), user.getPassword());
 
         commandUserPort.saveUser(user.setToken(request.deviceToken()));
 
