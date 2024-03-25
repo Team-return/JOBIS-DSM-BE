6edddbb40a5ba286f698aad6ddb5b6f9b640a1d3
gilgeunwoo <kesksjxhx@gmail.com>
2024-03-25 14:12:09 +0900

    🛠️ :: (#604) name 필드로

--- jobis-application/src/main/java/team/retum/jobis/domain/application/model/ApplicationStatus.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/application/model/ApplicationStatus.java
@@ -1,30 +1,24 @@
 package team.retum.jobis.domain.application.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
+@Getter
+@RequiredArgsConstructor
 public enum ApplicationStatus {
 
-    REQUESTED,
-    APPROVED,
-    SEND,
-    FAILED,
-    PASS,
-    FIELD_TRAIN,
-    ACCEPTANCE,
-    REJECTED;
+    REQUESTED("승인 요청"),
+    APPROVED("승인"),
+    SEND("기업에게 전송"),
+    FAILED("탈락"),
+    PASS("합격"),
+    FIELD_TRAIN("현장실습"),
+    ACCEPTANCE("근로계약"),
+    REJECTED("반려");
+
+    private final String name;
 
     public static final List<ApplicationStatus> DUPLICATE_CHECK = List.of(APPROVED, PASS, FIELD_TRAIN);
-
-    public String getName() {
-        return switch (this) {
-            case REQUESTED -> "승인 요청";
-            case APPROVED -> "승인";
-            case SEND -> "기업에게 전송";
-            case FAILED -> "탈락";
-            case PASS -> "합격";
-            case FIELD_TRAIN -> "현장실습";
-            case ACCEPTANCE -> "근로계약";
-            case REJECTED -> "거부";
-        };
-    }
 }
