ac19a6b69540ac0d19959a8e269cf5a50808c440
길근우 <kesksjxhx@gmail.com>
2024-03-25 10:08:19 +0900

    🧩 :: (#604) 지원서 상태 이름 반환

--- jobis-application/src/main/java/team/retum/jobis/domain/application/model/ApplicationStatus.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/application/model/ApplicationStatus.java
@@ -4,14 +4,27 @@ import java.util.List;
 
 public enum ApplicationStatus {
 
-    REQUESTED, // 승인 요청
-    APPROVED, // 승인
-    SEND, // 전송
-    FAILED, // 탈락
-    PASS, // 통과
-    FIELD_TRAIN, // 현장실습
-    ACCEPTANCE, // 근로계약
-    REJECTED; // 거부
+    REQUESTED,
+    APPROVED,
+    SEND,
+    FAILED,
+    PASS,
+    FIELD_TRAIN,
+    ACCEPTANCE,
+    REJECTED;
 
     public static final List<ApplicationStatus> DUPLICATE_CHECK = List.of(APPROVED, PASS, FIELD_TRAIN);
+
+    public String getName() {
+        return switch (this) {
+            case REQUESTED -> "승인 요청";
+            case APPROVED -> "승인";
+            case SEND -> "기업에게 전송";
+            case FAILED -> "탈락";
+            case PASS -> "합격";
+            case FIELD_TRAIN -> "현장실습";
+            case ACCEPTANCE -> "근로계약";
+            case REJECTED -> "거부";
+        };
+    }
 }
