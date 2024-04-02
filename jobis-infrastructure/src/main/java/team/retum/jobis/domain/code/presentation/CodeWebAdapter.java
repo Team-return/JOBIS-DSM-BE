0f6b30cbc92e0601f697bd2ba5ad6e5188debd21
Kangmin <ehfehf0629@gmail.com>
2024-04-03 08:56:15 +0900

    🧩 :: (#615) cache condition 추가

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/code/presentation/CodeWebAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/code/presentation/CodeWebAdapter.java
@@ -29,7 +29,7 @@ public class CodeWebAdapter {
     private final QueryCodesUseCase codesService;
     private final CreateCodeUseCase createCodeUseCase;
 
-    @Cacheable
+    @Cacheable(condition = "#keyword == null && #parentCode == null")
     @GetMapping
     public CodesResponse getCodes(
         @RequestParam(value = "type") CodeType codeType,
