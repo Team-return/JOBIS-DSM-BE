794d797b725ea32b289899d0a3a636a6f705eb45
4mjeo <4mj.eo@dsm.hs.kr>
2024-04-03 19:17:39 +0900

    🧩 :: (#617) notice event 추가

---
+++ jobis-infrastructure/src/main/java/team/retum/jobis/event/notice/NoticeEventHandler.java
@@ -0,0 +1,44 @@
+package team.retum.jobis.event.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import team.retum.jobis.domain.auth.model.Authority;
+import team.retum.jobis.domain.notice.event.NoticePostedEvent;
+import team.retum.jobis.domain.notification.model.Notification;
+import team.retum.jobis.domain.notification.model.Topic;
+import team.retum.jobis.domain.notification.spi.CommandNotificationPort;
+import team.retum.jobis.domain.user.model.User;
+import team.retum.jobis.domain.user.spi.QueryUserPort;
+import team.retum.jobis.thirdparty.fcm.FCMUtil;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class NoticeEventHandler {
+
+    private final CommandNotificationPort commandNotificationPort;
+    private final QueryUserPort queryUserPort;
+    private final FCMUtil fcmUtil;
+
+    @Async("asyncTaskExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onNoticePosted(NoticePostedEvent event) {
+        List<User> users = queryUserPort.queryAllUsers();
+
+        Notification notification = Notification.builder()
+                .title(event.getNotice().getTitle())
+                .content(event.getNotice().getContent())
+                .topic(Topic.NEW_NOTICE)
+                .detailId(event.getNotice().getId())
+                .authority(Authority.STUDENT)
+                .isNew(true)
+                .build();
+
+        commandNotificationPort.saveNotification(notification);
+        fcmUtil.sendMessageToTopic(notification);
+    }
+}
