4486e59266d39a57f71405f08fd8b492ca5aa8a2
4mjeo <4mj.eo@dsm.hs.kr>
2024-04-04 20:10:48 +0900

    🧩 :: (#617) 구독,해제 로직

--- jobis-application/src/main/java/team/retum/jobis/domain/notification/spi/CommandNotificationPort.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/notification/spi/CommandNotificationPort.java
@@ -1,8 +1,13 @@
 package team.retum.jobis.domain.notification.spi;
 
 import team.retum.jobis.domain.notification.model.Notification;
+import team.retum.jobis.domain.notification.model.Topic;
 
 public interface CommandNotificationPort {
 
     void saveNotification(Notification notification);
+
+    void unsubscribeTopic(String accountId, Topic topic);
+
+    void subscribeTopic(String accountId, Topic topic);
 }
--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/notification/persistence/NotificationPersistenceAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/notification/persistence/NotificationPersistenceAdapter.java
@@ -1,14 +1,19 @@
 package team.retum.jobis.domain.notification.persistence;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import team.retum.jobis.domain.notification.model.Notification;
+import team.retum.jobis.domain.notification.model.Topic;
 import team.retum.jobis.domain.notification.persistence.mapper.NotificationMapper;
 import team.retum.jobis.domain.notification.persistence.repository.NotificationJpaRepository;
 import team.retum.jobis.domain.notification.spi.NotificationPort;
+import team.retum.jobis.domain.user.persistence.entity.UserEntity;
+import team.retum.jobis.domain.user.persistence.repository.UserJpaRepository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,8 +25,10 @@ import static team.retum.jobis.domain.user.persistence.entity.QUserEntity.userEn
 public class NotificationPersistenceAdapter implements NotificationPort {
 
     private final NotificationJpaRepository notificationJpaRepository;
+    private final UserJpaRepository userJpaRepository;
     private final NotificationMapper notificationMapper;
     private final JPAQueryFactory queryFactory;
+    private final FirebaseMessaging firebaseInstance;
 
     @Override
     public void saveNotification(Notification notification) {
@@ -47,6 +54,30 @@ public class NotificationPersistenceAdapter implements NotificationPort {
             .fetch().stream()
             .map(notificationMapper::toDomain)
             .toList();
+    }
+
+    @Override
+    public void subscribeTopic(String accountId, Topic topic) {
+        try {
+            firebaseInstance.subscribeToTopicAsync(Arrays.asList(accountId), topic.toString()).get();
+            UserEntity userEntity = userJpaRepository.findByAccountId(accountId).orElseThrow();
+            userEntity.setIsSubscribed(true);
+            userJpaRepository.save(userEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void unsubscribeTopic(String accountId, Topic topic) {
+        try {
+            firebaseInstance.unsubscribeFromTopicAsync(Arrays.asList(accountId), topic.toString()).get();
+            UserEntity userEntity = userJpaRepository.findByAccountId(accountId).orElseThrow();
+            userEntity.setIsSubscribed(false);
+            userJpaRepository.save(userEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //==condition==//
