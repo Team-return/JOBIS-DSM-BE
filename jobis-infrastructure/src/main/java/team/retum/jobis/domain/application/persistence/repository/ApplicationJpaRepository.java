60235a3d4339cea8c1343d7cd9cc39080e32c606
이하성 <tedsoftj1123@gmail.com>
2024-04-15 18:53:30 +0900

    🦷 :: (#634) rewrite run

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/application/persistence/repository/ApplicationJpaRepository.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/application/persistence/repository/ApplicationJpaRepository.java
@@ -2,7 +2,6 @@ package team.retum.jobis.domain.application.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import team.retum.jobis.domain.application.model.ApplicationStatus;
 import team.retum.jobis.domain.application.persistence.entity.ApplicationEntity;
 
 import java.util.List;
