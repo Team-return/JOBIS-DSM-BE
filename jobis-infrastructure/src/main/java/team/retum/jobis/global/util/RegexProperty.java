8f30da6aa3424ed28df570381365abbb54190f07
이하성 <tedsoftj1123@gmail.com>
2024-05-11 22:40:34 +0900

    ⚙️ :: (#develop) pre-signed validation

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/util/RegexProperty.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/util/RegexProperty.java
@@ -10,4 +10,6 @@ public class RegexProperty {
     public static final String PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$";
 
     public static final String WORKING_HOURS = "^([01][0-9]|2[0-3]):([0-5][0-9]) ~ ([01][0-9]|2[0-3]):([0-5][0-9])$";
+
+    public static final String FILE_NAME = "^.+\\..+$";
 }
