005fb06ae442d461f3bc3cf8a29cfc5077705ed8
4mjeo <4mj.eo@dsm.hs.kr>
2024-05-11 19:52:21 +0900

    🧩 :: (#663) 예외처리

--- jobis-application/src/main/java/team/retum/jobis/domain/application/usecase/TeacherDeleteApplicationUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/application/usecase/TeacherDeleteApplicationUseCase.java
@@ -3,6 +3,7 @@ package team.retum.jobis.domain.application.usecase;
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.UseCase;
 import team.retum.jobis.domain.application.spi.CommandApplicationPort;
+import team.retum.jobis.domain.application.spi.QueryApplicationPort;
 
 import java.util.List;
 
@@ -11,8 +12,11 @@ import java.util.List;
 public class TeacherDeleteApplicationUseCase {
 
     private final CommandApplicationPort commandApplicationPort;
+    private final QueryApplicationPort queryApplicationPort;
 
     public void execute(List<Long> applicationIds) {
+        queryApplicationPort.getAllByIdInOrThrow(applicationIds);
+
         commandApplicationPort.deleteByIds(applicationIds);
     }
 }
