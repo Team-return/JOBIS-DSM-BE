e3a0777d7ed09eb69019c7344b1958ca250da433
4mjeo <4mj.eo@dsm.hs.kr>
2024-09-01 21:58:48 +0900

    🛠️ :: (#708) 빌드 에러 해결

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/wintern/presentation/WinternWebAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/wintern/presentation/WinternWebAdapter.java
@@ -2,7 +2,11 @@ package team.retum.jobis.domain.wintern.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import team.retum.jobis.domain.wintern.usecase.QueryIsWinternUseCase;
 import team.retum.jobis.domain.wintern.usecase.ToggleWinternUseCase;
 
