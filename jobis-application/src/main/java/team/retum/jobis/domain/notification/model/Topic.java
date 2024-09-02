88c13bcf47b46c9db8a9943d64d58ed636c86965
tae.y <0211ilyoil@gmail.com>
2024-09-02 11:36:08 +0900

    🦷 :: (#715) enum 삭제

--- jobis-application/src/main/java/team/retum/jobis/domain/notification/model/Topic.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/notification/model/Topic.java
@@ -2,12 +2,8 @@ package team.retum.jobis.domain.notification.model;
 
 public enum Topic {
 
-    //RECRUITMENT_STATUS_CHANGED,
-    //NEW_APPLICATION,
     APPLICATION_STATUS_CHANGED,
-    //INTERVIEW_SOON,
-    //NEW_RECRUITMENT,
     RECRUITMENT_DONE,
     NEW_NOTICE,
-    //NEW_INTERESTED_RECRUITMENT
+    NEW_INTERESTED_RECRUITMENT
 }
