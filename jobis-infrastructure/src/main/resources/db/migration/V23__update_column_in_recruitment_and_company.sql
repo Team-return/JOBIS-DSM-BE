71bbe659b2f882f826e0c5d6f235efb78b5fd9f9
김명진 <4mj.eo@dsm.hs.kr>
2024-11-04 23:18:13 +0900

    🛠 :: (#816) nullable로 변경

--- jobis-infrastructure/src/main/resources/db/migration/V23__update_column_in_recruitment_and_company.sql
+++ jobis-infrastructure/src/main/resources/db/migration/V23__update_column_in_recruitment_and_company.sql
@@ -3,3 +3,5 @@ alter table tbl_recruitment add column integration_plan tinyint(1) not null;
 alter table tbl_company rename column representative_phone_no to manager_phone_no;
 
 alter table tbl_company add column representative_phone_no varchar(12);
+
+alter table tbl_recruitment modify column integration_plan tinyint(1) null;
