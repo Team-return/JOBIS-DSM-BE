af9175e3b94a581c8f0b0137c39fc56a0e7dbcc1
김명진 <4mj.eo@dsm.hs.kr>
2025-03-12 22:42:24 +0900

    ⚙️ :: (#884) rabbitmq 메세지 컨버터 추가

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RabbitMqConfig.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RabbitMqConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +29,17 @@ public class RabbitMqConfig {
         return BindingBuilder.bind(notificationQueue)
             .to(notificationExchange)
             .with("notification.#");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 }
