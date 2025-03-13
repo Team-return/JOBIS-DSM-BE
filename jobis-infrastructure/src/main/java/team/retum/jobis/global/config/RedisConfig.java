0ab95ea934f2261b1e4a66930e5b609d60b04697
tae.y <0211ilyoil@gmail.com>
2025-03-13 22:28:36 +0900

    🦷 :: (#896) redisConfig 복구

---
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package team.retum.jobis.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@RequiredArgsConstructor
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.port}")
+    private Integer port;
+
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        configuration.setPassword(password);
+        return new LettuceConnectionFactory(configuration);
+    }
+}
