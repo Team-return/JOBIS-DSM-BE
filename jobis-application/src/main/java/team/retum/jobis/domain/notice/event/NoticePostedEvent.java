057d2c0364918e4b113b394506bbc61ae9b368ca
김명진 <4mj.eo@dsm.hs.kr>
2025-03-13 22:13:21 +0900

    ⚙️ :: 직렬화 위한 기본생성자 추가

--- jobis-application/src/main/java/team/retum/jobis/domain/notice/event/NoticePostedEvent.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/notice/event/NoticePostedEvent.java
@@ -2,11 +2,13 @@ package team.retum.jobis.domain.notice.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import team.retum.jobis.domain.notice.model.Notice;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NoticePostedEvent {
 
-    private final Notice notice;
+    private Notice notice;
 }
