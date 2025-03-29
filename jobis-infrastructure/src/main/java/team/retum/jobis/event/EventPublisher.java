4b960c399449df555f0e60b2d3f522279dd4224f
김명진 <4mj.eo@dsm.hs.kr>
2025-03-29 13:09:54 +0900

    🛠 :: (#920) 기본 어노테이션 추가

--- jobis-infrastructure/src/main/java/team/retum/jobis/event/EventPublisher.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/event/EventPublisher.java
@@ -2,9 +2,11 @@ package team.retum.jobis.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import team.retum.jobis.common.spi.PublishEventPort;
 
+@Primary
 @RequiredArgsConstructor
 @Component
 public class EventPublisher implements PublishEventPort {
