a8cb6c334be7205d0f8c55adac87e5e693197dca
tae.y <0211ilyoil@gmail.com>
2025-04-02 09:14:51 +0900

    🛠 :: (#930) 스케줄러 시간 수정

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/scheduler/FcmScheduler.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/scheduler/FcmScheduler.java
@@ -15,7 +15,7 @@ public class FcmScheduler {
      * 매 4월 2일 9시 15분에 실행
      * deviceToken을 가지고 있는 유저들의 모든 토픽구독 상태를 true로 변경
      */
-    @Scheduled(cron = "0 15 9 2 4 *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 25 9 2 4 *", zone = "Asia/Seoul")
     public void subscribeAllUser() {
         subscribeAllUserUseCase.execute();
     }
