83a591de41e567029cbe392241476d17c639e98b
김경민 <kkm07133@naver.com>
2025-06-15 16:05:14 +0900

    🛠 :: (#975) notice JpaRepository로 변경

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/notice/persistence/mapper/ViewLogMapper.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/notice/persistence/mapper/ViewLogMapper.java
@@ -2,10 +2,10 @@ package team.retum.jobis.domain.notice.persistence.mapper;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import team.retum.jobis.domain.notice.persistence.entity.NoticeEntity;
-import team.retum.jobis.domain.notice.persistence.repository.NoticeJpaRepository;
 import team.retum.jobis.domain.notice.model.ViewLog;
+import team.retum.jobis.domain.notice.persistence.entity.NoticeEntity;
 import team.retum.jobis.domain.notice.persistence.entity.ViewLogEntity;
+import team.retum.jobis.domain.notice.persistence.repository.NoticeJpaRepository;
 import team.retum.jobis.domain.student.persistence.entity.StudentEntity;
 import team.retum.jobis.domain.student.persistence.repository.StudentJpaRepository;
 
@@ -14,14 +14,11 @@ import team.retum.jobis.domain.student.persistence.repository.StudentJpaReposito
 public class ViewLogMapper {
 
     private final NoticeJpaRepository noticeJpaRepository;
-
     private final StudentJpaRepository studentJpaRepository;
 
-    private final
-
     public ViewLogEntity toEntity(ViewLog viewLog) {
-        NoticeEntity notice = noticeJpaRepository.findById(viewLog.getNoticeId());
-        StudentEntity student = studentJpaRepository.findById(viewLog.getUserId());
+        NoticeEntity notice = noticeJpaRepository.getReferenceById(viewLog.getNoticeId());
+        StudentEntity student = studentJpaRepository.getReferenceById(viewLog.getStudentId());
 
         return ViewLogEntity.builder()
                 .notice(notice)
@@ -30,12 +27,11 @@ public class ViewLogMapper {
                 .build();
     }
 
-
     public ViewLog toDomain(ViewLogEntity entity) {
         return ViewLog.builder()
                 .id(entity.getId())
-                .noticeId(entity.getNotificationId())
-                .userId(entity.getUserId())
+                .noticeId(entity.getNotice().getId())
+                .studentId(entity.getStudent().getId())
                 .viewedAt(entity.getViewedAt())
                 .build();
     }
