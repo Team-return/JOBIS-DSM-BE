f681c6909acd177f7f17487f1d379647222f1fe6
uyjeamin <kariel7673@gmail.com>
2025-08-04 23:31:45 +0900

    🦷 :: (#992) @Repository 어노테이션 제거

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/review/persistence/repository/QuestionJpaRepository.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/review/persistence/repository/QuestionJpaRepository.java
@@ -1,9 +1,7 @@
 package team.retum.jobis.domain.review.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import team.retum.jobis.domain.review.persistence.entity.QuestionEntity;
 
-@Repository
 public interface QuestionJpaRepository extends JpaRepository<QuestionEntity, Long> {
 }
