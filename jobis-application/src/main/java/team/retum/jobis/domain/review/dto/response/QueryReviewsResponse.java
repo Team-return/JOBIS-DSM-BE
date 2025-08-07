9c89f17895254a733a92f585473a22e563d67d2e
민수아 <Minsua0503@gmail.com>
2025-08-07 16:39:55 +0900

    ✨ :: (#988) 리뷰 VO 및 response 값 개편

--- jobis-application/src/main/java/team/retum/jobis/domain/review/dto/response/QueryReviewsResponse.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/review/dto/response/QueryReviewsResponse.java
@@ -1,6 +1,7 @@
 package team.retum.jobis.domain.review.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import team.retum.jobis.domain.review.spi.vo.ReviewVO;
 
@@ -10,6 +11,30 @@ import java.util.List;
 @AllArgsConstructor
 public class QueryReviewsResponse {
 
-    private final List<ReviewVO> reviews;
+    private final List<ReviewResponse> reviews;
 
+    @Getter
+    @Builder
+    public static class ReviewResponse {
+
+        private Long reviewId;
+
+        private String companyName;
+
+        private String writer;
+
+        private int year;
+
+        private String major;
+
+        public static ReviewResponse from(ReviewVO vo) {
+            return ReviewResponse.builder()
+                .reviewId(vo.getReviewId())
+                .companyName(vo.getCompanyName())
+                .writer(vo.getWriter())
+                .year(vo.getYear())
+                .major(vo.getMajor())
+                .build();
+        }
+    }
 }
