62e56e4d701bdc2317e7be9470180293748bc723
민수아 <Minsua0503@gmail.com>
2025-08-08 20:23:50 +0900

    🛠 :: (#1002) 생성자 방식 변경

--- jobis-application/src/main/java/team/retum/jobis/domain/review/spi/vo/ReviewVO.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/review/spi/vo/ReviewVO.java
@@ -1,23 +1,19 @@
 package team.retum.jobis.domain.review.spi.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
+@AllArgsConstructor
 public class ReviewVO {
 
     private final Long reviewId;
-    private final String companyName;
-    private final String writer;
-    private final int year;
-    private final String major;
 
-    public ReviewVO(Long reviewId, String companyName, String writer, LocalDateTime createdAt, String major) {
-        this.reviewId = reviewId;
-        this.companyName = companyName;
-        this.writer = writer;
-        this.year = createdAt.getYear();
-        this.major = major;
-    }
+    private final String companyName;
+
+    private final String writer;
+
+    private final int year;
+
+    private final String major;
 }
