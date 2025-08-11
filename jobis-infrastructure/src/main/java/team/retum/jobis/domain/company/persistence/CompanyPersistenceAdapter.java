2259d834fbeadbc6f76adc235988a1d31331b1a6
민수아 <Minsua0503@gmail.com>
2025-08-11 14:00:33 +0900

    🛠 :: (#1009) 구간 주석 추가

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/company/persistence/CompanyPersistenceAdapter.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/company/persistence/CompanyPersistenceAdapter.java
@@ -346,6 +346,8 @@ public class CompanyPersistenceAdapter implements CompanyPort {
             .fetch();
     }
 
+    //==condition==//
+
     private BooleanExpression containsName(String name) {
         return name == null ? null : companyEntity.name.contains(name);
     }
