170f66ef7b2583b81ab7112a3b67b1c2ab3879f2
민수아 <Minsua0503@gmail.com>
2025-10-29 09:52:40 +0900

    ✨ :: (#1044) 테스트 컨테이너 초기 세팅

---
+++ jobis-infrastructure/src/test/java/team/retum/jobis/config/IntegrationTest.java
@@ -0,0 +1,33 @@
+package team.retum.jobis.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static team.retum.jobis.config.TestContainerConfig.MYSQL_CONTAINER;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@Import(TestContainerConfig.class)
+public abstract class IntegrationTest {
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.jpa.show-sql", () -> "true");
+        registry.add("spring.jpa.properties.hibernate.format_sql", () -> "true");
+        registry.add("spring.jpa.properties.hibernate.dialect",
+            () -> "org.hibernate.dialect.MySQLDialect");
+
+        registry.add("spring.flyway.enabled", () -> "false");
+    }
+}
