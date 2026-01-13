2e8b1b3c0061ff80c6e93e7dbe0285a4c4710c00
민수아 <Minsua0503@gmail.com>
2026-01-13 22:23:33 +0900

    ✨ :: (#1074) Interview, DocumentNumber Repository 추가

---
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/interview/persistence/repository/InterviewJpaRepository.java
@@ -0,0 +1,9 @@
+package team.retum.jobis.domain.interview.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.retum.jobis.domain.interview.persistence.entity.InterviewEntity;
+
+public interface InterviewJpaRepository extends JpaRepository<InterviewEntity, Long> {
+    
+}
+
