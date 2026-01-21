f38e746b1b84e122789b669abd90d08a4c013cd1
kkm06100 <kkm06101010@gmail.com>
2026-01-21 16:16:48 +0900

    🛠 :: (#1083) Size 및 flyway 번호 수정

--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/banner/presentation/dto/CreateBannerWebRequest.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/banner/presentation/dto/CreateBannerWebRequest.java
@@ -1,6 +1,7 @@
 package team.retum.jobis.domain.banner.presentation.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,9 +15,11 @@ import java.time.LocalDate;
 public class CreateBannerWebRequest {
 
     @NotNull
+    @Size(max = 100)
     private String title;
 
     @NotNull
+    @Size(max = 500)
     private String content;
 
     @NotNull
