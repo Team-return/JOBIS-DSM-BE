9797e6efa34a71cca1129c4fcb76dd68392a3eed
민수아 <Minsua0503@gmail.com>
2026-02-04 21:32:39 +0900

    🐛 :: (#1088) DB 생성 시 Default 값 에러 수정

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/util/ImageProperty.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/util/ImageProperty.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ImageProperty {
 
-    public static final String DEFAULT_STUDENT_PROFILE_IMAGE = "EXTENSION_FILE/default_image.png";
+    public static final String DEFAULT_STUDENT_PROFILE_IMAGE = "'EXTENSION_FILE/default_image.png'";
 
-    public static final String DEFAULT_COMPANY_LOGO_IMAGE = "LOGO_IMAGE/companydefault.png";
+    public static final String DEFAULT_COMPANY_LOGO_IMAGE = "'LOGO_IMAGE/companydefault.png'";
 }
