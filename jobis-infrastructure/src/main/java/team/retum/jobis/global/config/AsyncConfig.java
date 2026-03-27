34e1c16a1848b617645acd1e5f3665b70c3f18f5
treegus <cowtree28@gmail.com>
2026-03-27 20:05:33 +0900

    ✨ :: (#1127) 사용하지 않는 import문 삭제

--- jobis-infrastructure/src/main/java/team/retum/jobis/global/config/AsyncConfig.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/global/config/AsyncConfig.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
 @Configuration
