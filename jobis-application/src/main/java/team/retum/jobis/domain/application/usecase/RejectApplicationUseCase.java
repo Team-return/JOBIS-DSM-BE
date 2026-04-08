206237c4bf7c446b8705a89913b405f3cbb5901b
yellowstarr0323 <ihansaem60@gmail.com>
2026-04-08 10:23:00 +0900

    🪄 :: (#1132) 사용되지 않는 import문 삭제

--- jobis-application/src/main/java/team/retum/jobis/domain/application/usecase/RejectApplicationUseCase.java
+++ jobis-application/src/main/java/team/retum/jobis/domain/application/usecase/RejectApplicationUseCase.java
@@ -3,14 +3,11 @@ package team.retum.jobis.domain.application.usecase;
 import lombok.RequiredArgsConstructor;
 import team.retum.jobis.common.annotation.UseCase;
 import team.retum.jobis.common.spi.PublishEventPort;
-import team.retum.jobis.domain.application.dto.request.RejectionAttachmentRequest;
 import team.retum.jobis.domain.application.event.SingleApplicationStatusChangedEvent;
 import team.retum.jobis.domain.application.model.Application;
 import team.retum.jobis.domain.application.model.ApplicationStatus;
-import team.retum.jobis.domain.application.model.ApplicationRejectionAttachment;
 import team.retum.jobis.domain.application.spi.CommandApplicationPort;
 import team.retum.jobis.domain.application.spi.QueryApplicationPort;
-import java.util.List;
 
 @RequiredArgsConstructor
 @UseCase
--- jobis-infrastructure/src/main/java/team/retum/jobis/domain/application/presentation/dto/request/RejectApplicationWebRequest.java
+++ jobis-infrastructure/src/main/java/team/retum/jobis/domain/application/presentation/dto/request/RejectApplicationWebRequest.java
@@ -1,7 +1,6 @@
 package team.retum.jobis.domain.application.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
